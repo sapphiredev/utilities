abae927f5a5dbf1785770569d0fc6bcdf658880e
Jeroen Claassens <support@favware.tech>
2020-08-22 03:24:08 +0200

    fix(utilities): fixed types for codeBlock

--- packages/utilities/src/lib/codeBlock.ts
+++ packages/utilities/src/lib/codeBlock.ts
@@ -6,7 +6,10 @@ const zws = String.fromCharCode(8203);
  * @param language The codeblock language
  * @default 'md'
  */
-export function codeBlock(expression: string, language = 'md'): string {
-	if (expression.length === 0) return `\`\`\`${zws}\`\`\``;
-	return `\`\`\`${language}\n${expression.replace(/```/, `\`${zws}\`\``).replace(/`$/g, `\`${zws}`)}\`\`\``;
+export function codeBlock<T extends unknown>(expression: T, language = 'md'): string {
+	if (typeof expression === 'string') {
+		if (expression.length === 0) return `\`\`\`${zws}\`\`\``;
+		return `\`\`\`${language}\n${expression.replace(/```/, `\`${zws}\`\``).replace(/`$/g, `\`${zws}`)}\`\`\``;
+	}
+	return `\`\`\`${language}\n${expression || zws}\`\`\``;
 }
