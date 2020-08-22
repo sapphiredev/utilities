54bfba781ac7d6f9d60fc7fec85475d8496d9aa2
Jeroen Claassens <support@favware.tech>
2020-08-22 03:26:59 +0200

    fix(utilities): put lang as first param

--- packages/utilities/src/lib/codeBlock.ts
+++ packages/utilities/src/lib/codeBlock.ts
@@ -2,11 +2,10 @@ const zws = String.fromCharCode(8203);
 
 /**
  * Wraps text in a markdown codeblock with optionally a language indicator for syntax highlighting
- * @param expression The expression to be wrapped in the codeblock
  * @param language The codeblock language
- * @default 'md'
+ * @param expression The expression to be wrapped in the codeblock
  */
-export function codeBlock<T extends unknown>(expression: T, language = 'md'): string {
+export function codeBlock<T extends unknown>(language: string, expression: T): string {
 	if (typeof expression === 'string') {
 		if (expression.length === 0) return `\`\`\`${zws}\`\`\``;
 		return `\`\`\`${language}\n${expression.replace(/```/, `\`${zws}\`\``).replace(/`$/g, `\`${zws}`)}\`\`\``;
--- packages/utilities/tests/codeBlock.test.ts
+++ packages/utilities/tests/codeBlock.test.ts
@@ -5,22 +5,25 @@ const zeroWidthSpace = String.fromCharCode(8203);
 
 describe('codeBlock', () => {
 	test('GIVEN expression w/o length THEN returns wrapped ZeroWidthSpace', () => {
-		expect(codeBlock('')).toStrictEqual(`\`\`\`${zeroWidthSpace}\`\`\``);
+		expect(codeBlock('md', '')).toStrictEqual(`\`\`\`${zeroWidthSpace}\`\`\``);
 	});
 
 	test('GIVEN expression w/ length THEN returns expressed wrapped in markdown', () => {
-		expect(codeBlock('# Header')).toStrictEqual(`\`\`\`md
+		expect(codeBlock('md', '# Header')).toStrictEqual(`\`\`\`md
 # Header\`\`\``);
 	});
 
 	test('GIVEN expression w/ length w/ inner code block THEN returns expressed wrapped in markdown', () => {
 		expect(
-			codeBlock(`
+			codeBlock(
+				'md',
+				`
 			# Header
 			\`\`\`js
 				if (sapphireProjectIsCool) return 'awesome!';
 			\`\`\`
-		`)
+		`
+			)
 		).toStrictEqual(`\`\`\`md
 
 			# Header
@@ -32,10 +35,13 @@ describe('codeBlock', () => {
 
 	test('GIVEN expression w/ length w/ inner inline code block THEN returns expressed wrapped in markdown', () => {
 		expect(
-			codeBlock(`
+			codeBlock(
+				'md',
+				`
 			# Header
 			\`const sapphireProjectIsCool = true\`
-		`)
+		`
+			)
 		).toStrictEqual(`\`\`\`md
 
 			# Header
