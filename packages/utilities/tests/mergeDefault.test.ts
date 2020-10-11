269fba1be36350aaa1399648cf074d955272c8d3
Jeroen Claassens <support@favware.tech>
2020-10-12 00:03:30 +0200

    fix(utilities): fixed up mergeDefault

--- packages/utilities/tests/mergeDefault.test.ts
+++ packages/utilities/tests/mergeDefault.test.ts
@@ -1,55 +1,70 @@
 import { mergeDefault } from '../src';
 
 describe('mergeDefault', () => {
-	test('GIVEN basic THEN returns expected', () => {
-		const defaults = { a: 0, b: 1 };
-		const given = {};
-		expect(mergeDefault(defaults, given)).toEqual({ a: 0, b: 1 });
+	test('GIVEN empty overwrites THEN returns base', () => {
+		const base = { a: 0, b: 1 };
+		const overwrites = {};
+		expect(mergeDefault(base, overwrites)).toStrictEqual(base);
 	});
 
-	test('GIVEN mutation THEN returns expected', () => {
-		expect.assertions(2);
+	test('GIVEN empty overwrites THEN returns mutates overwrites', () => {
+		const base = { a: 0, b: 1 };
+		const overwrites = {};
 
-		const defaults = { a: 0, b: 1 };
-		const given = {};
-		mergeDefault(defaults, given);
+		mergeDefault(base, overwrites);
 
-		expect(defaults).toEqual({ a: 0, b: 1 });
-		expect(given).toEqual({ a: 0, b: 1 });
+		expect(base).toStrictEqual({ a: 0, b: 1 });
+		expect(overwrites).toStrictEqual({ a: 0, b: 1 });
 	});
 
-	test('GIVEN clone THEN returns expected', () => {
-		const defaults = { a: 0, b: 1 };
-		expect(mergeDefault(defaults)).toEqual({ a: 0, b: 1 });
+	test('GIVEN undefined overwrites THEN returns clones base', () => {
+		const base = { a: 0, b: 1 };
+		expect(mergeDefault(base)).toStrictEqual({ a: 0, b: 1 });
 	});
 
-	test('GIVEN partial THEN returns expected', () => {
-		const defaults = { a: 0, b: 1 };
-		const given = { a: 2 };
-		expect(mergeDefault(defaults, given)).toEqual({ a: 2, b: 1 });
+	test('GIVEN partial overwrites THEN returns merged object', () => {
+		const base = { a: 0, b: 1 };
+		const overwrites = { a: 2 };
+		expect(mergeDefault(base, overwrites)).toStrictEqual({ a: 2, b: 1 });
 	});
 
-	test('GIVEN extended THEN returns expected', () => {
-		const defaults = { a: 0, b: 1 };
-		const given = { a: 2, i: 3 };
-		expect(mergeDefault(defaults, given)).toEqual({ a: 2, i: 3, b: 1 });
+	test('GIVEN extended overwrites THEN returns merged object', () => {
+		const base = { a: 0, b: 1 };
+		const overwrites = { a: 2, i: 3 };
+		expect(mergeDefault(base, overwrites)).toStrictEqual({ a: 2, i: 3, b: 1 });
 	});
 
-	test('GIVEN partial-falsy-null THEN returns expected', () => {
-		const defaults: { a: null | number; b: number } = { a: 0, b: 1 };
-		const given = { a: null };
-		expect(mergeDefault(defaults, given)).toEqual({ a: null, b: 1 });
+	test('GIVEN partial-null overwrites THEN returns null plus base keys', () => {
+		interface Sample {
+			a: null | number;
+			b: number;
+		}
+
+		const base: Sample = { a: 0, b: 1 };
+		const overwrites: Partial<Sample> = { a: null };
+		expect(mergeDefault(base, overwrites)).toStrictEqual({ a: null, b: 1 });
 	});
 
-	test('GIVEN partial-undefined THEN returns expected', () => {
-		const defaults = { a: 0, b: 1 };
-		const given = { a: undefined };
-		expect(mergeDefault(defaults, given)).toEqual({ a: 0, b: 1 });
+	test('GIVEN partial-undefined overwrites THEN returns base value plus base keys', () => {
+		const base = { a: 0, b: 1 };
+		const overwrites = { a: undefined };
+		expect(mergeDefault(base, overwrites)).toStrictEqual({ a: 0, b: 1 });
 	});
 
-	test('GIVEN deep THEN returns expected', () => {
-		const defaults = { a: { b: 1 } };
-		const given = { a: { b: 2 } };
-		expect(mergeDefault(defaults, given)).toEqual({ a: { b: 2 } });
+	test('GIVEN deep object THEN returns all keys', () => {
+		const base = { a: { b: 1 } };
+		const overwrites = { a: { b: 2 } };
+		expect(mergeDefault(base, overwrites)).toStrictEqual({ a: { b: 2 } });
+	});
+
+	test('GIVEN partial-null base and provided overwrites THEN returns overwrites values', () => {
+		interface Sample {
+			a: null | { b: number };
+		}
+
+		const base: Sample = { a: null };
+		const overwrites: Sample = { a: { b: 5 } };
+
+		expect(mergeDefault(base, overwrites)).toStrictEqual({ a: { b: 5 } });
 	});
 });
