f50e25d54eec4bea15ecb6ef13ca8e70d8226038
Jeroen Claassens <support@favware.tech>
2020-12-22 11:26:44 +0100

    chore: don’t mess with voodoo

--- packages/discord-utilities/src/lib/twemojiLib.d.ts
+++ packages/discord-utilities/src/lib/twemojiLib.d.ts
@@ -2,7 +2,6 @@ declare module 'twemoji-parser/dist/lib/regex' {
 	/**
 	 * Regex that can capture a Twemoji (Twitter Emoji)
 	 * @raw {@link https://github.com/twitter/twemoji-parser/blob/master/src/lib/regex.js See official source code}
-	 * @remark Capture group 1 is the Twemoji name. It is named `name`.
 	 */
 	const regex: string;
 
