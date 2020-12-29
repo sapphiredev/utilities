02ac397b6c45bef4c9dd9016334fa693c1638ba5
Hezekiah Hendry <46537907+RealShadowNova@users.noreply.github.com>
2020-12-29 15:36:55 -0500

    fix: forgot a semi

--- packages/discord.js-utilities/src/index.ts
+++ packages/discord.js-utilities/src/index.ts
@@ -1,4 +1,4 @@
 export * from '@sapphire/discord-utilities';
 export * from './lib/type-guards';
 export * from './lib/PaginatedMessage';
-export * from './lib/LazyPaginatedMessage'
+export * from './lib/LazyPaginatedMessage';
