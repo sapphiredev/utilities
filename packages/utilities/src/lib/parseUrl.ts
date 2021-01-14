718065a34b5ee225503f8b2197784f1fd1b15f19
Jeroen Claassens <support@favware.tech>
2021-01-14 19:58:59 +0100

    fix(utilities): fixed return type for parseURL
    It is now properly `URL | null` instead of `any`

--- packages/utilities/src/lib/parseUrl.ts
+++ packages/utilities/src/lib/parseUrl.ts
@@ -2,9 +2,8 @@
  * Parses an URL, returns null if invalid.
  * @param url The url to parse
  */
-export function parseURL(url: string) {
+export function parseURL(url: string): URL | null {
 	try {
-		// @ts-expect-error URL is globally available in NodeJS and browsers
 		return new URL(url);
 	} catch {
 		return null;
