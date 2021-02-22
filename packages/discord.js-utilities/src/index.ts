338c36acd50941ba35b8cf84a8998f808f1d8b2a
Jurien Hamaker <jurien@kings-dev.io>
2021-02-22 08:13:31 +0100

    chore: save merge conflict resolution

--- packages/discord.js-utilities/src/index.ts
+++ packages/discord.js-utilities/src/index.ts
@@ -1,9 +1,6 @@
 export * from '@sapphire/discord-utilities';
 export * from './lib/builders/MessageBuilder';
 export * from './lib/LazyPaginatedMessage';
-<<<<<<< HEAD
 export * from './lib/MessagePrompter';
-=======
 export * from './lib/PaginatedMessage';
 export * from './lib/type-guards';
->>>>>>> main
