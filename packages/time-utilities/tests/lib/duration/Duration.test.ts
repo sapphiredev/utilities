f19aa1aa9810975621238aaff58508147e3a8697
Antonio Román <kyradiscord@gmail.com>
2021-02-23 16:49:28 +0100

    fix(duration): make invalid input return NaN

--- packages/time-utilities/tests/lib/duration/Duration.test.ts
+++ packages/time-utilities/tests/lib/duration/Duration.test.ts
@@ -1,14 +1,19 @@
 import { Duration } from '../../../src';
 
 describe('Duration', () => {
+	test('GIVEN duration with an offset of 0s, THEN shows 0ms', () => {
+		const duration = new Duration('0s');
+		expect(duration.offset).toEqual(0);
+	});
+
 	test('GIVEN duration with an offset of 1s, THEN shows 1000ms', () => {
 		const duration = new Duration('a second');
 		expect(duration.offset).toEqual(1000);
 	});
 
-	test('GIVEN invalid duration THEN show zero offset', () => {
+	test('GIVEN invalid duration THEN show NaN', () => {
 		const duration = new Duration('foo');
-		expect(duration.offset).toEqual(0);
+		expect(duration.offset).toEqual(NaN);
 	});
 
 	test('GIVEN duration with offset, THEN dateFrom is valid', () => {
