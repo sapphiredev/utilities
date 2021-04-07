740936fcf5a05559656f363f094501448ad27eb0
Gryffon Bellish <owenbellish@gmail.com>
2021-04-07 20:24:47 +0000

    impl done, need to do docs

---
+++ packages/embed-jsx/src/lib/EmbedJsx.ts
@@ -0,0 +1,97 @@
+import { MessageEmbed, ColorResolvable } from 'discord.js';
+
+const EMBED_TYPES = ['embed', 'title', 'field', 'timestamp', 'footer', 'description', 'url'] as const;
+
+type EmbedInformation = TitleInformation | FieldInformation | TimestampInformation | FooterInformation | DescriptionInformation | UrlInformation;
+
+type EmbedData = TitleData | FieldData | TimestampData | FooterData | DescriptionData | UrlData;
+
+const enum TsxTypes {
+	Title,
+	Description,
+	Url,
+	Field,
+	Timestamp,
+	Footer
+}
+
+// Received types
+type TitleInformation = [null, string];
+type DescriptionInformation = [null, string];
+type UrlInformation = [null, string];
+type FieldInformation = [{ title?: string; inline?: boolean }, string];
+type TimestampInformation = [null, number | string | Date | null];
+type FooterInformation = [null | { iconURL?: string }, string];
+type EmbedInitialInformation = [{ color: ColorResolvable }, ...EmbedData[]];
+
+// Returned types
+type TitleData = [TsxTypes.Title, string];
+type DescriptionData = [TsxTypes.Description, string];
+type UrlData = [TsxTypes.Url, string];
+type FieldData = [TsxTypes.Field, string, string, boolean];
+type TimestampData = [TsxTypes.Timestamp, Date];
+type FooterData = [TsxTypes.Footer, string, string?];
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace EmbedJsx {
+	export function make(type: typeof EMBED_TYPES[number], ...data: EmbedInformation | EmbedInitialInformation): EmbedData | MessageEmbed {
+		if (!EMBED_TYPES.includes(type)) throw new TypeError(`Invalid type passed, expected one of ${EMBED_TYPES.join(', ')}, got: ${type}`);
+		switch (type) {
+			case 'embed': {
+				const info = data as EmbedInitialInformation;
+				let embed = new MessageEmbed(info[0]);
+				for (const value of info.splice(0)) {
+					embed = resolveData(value as EmbedData, embed);
+				}
+				return embed;
+			}
+			case 'title': {
+				const info = data as TitleInformation;
+				return [TsxTypes.Title, info[1]];
+			}
+			case 'field': {
+				const info = data as FieldInformation;
+				return [TsxTypes.Field, info[0].title ?? '\u200B', info[1], info[0].inline ?? false];
+			}
+			case 'footer': {
+				const info = data as FooterInformation;
+				return [TsxTypes.Footer, info[1], info[0]?.iconURL];
+			}
+			case 'timestamp': {
+				const info = data as TimestampInformation;
+				return [TsxTypes.Timestamp, new Date(info[1] ?? Date.now())];
+			}
+			case 'description': {
+				const info = data as DescriptionInformation;
+				return [TsxTypes.Description, info[1]];
+			}
+			case 'url': {
+				const info = data as UrlInformation;
+				return [TsxTypes.Url, info[1]];
+			}
+		}
+	}
+
+	function resolveData(data: EmbedData, embed: MessageEmbed) {
+		switch (data[0]) {
+			case TsxTypes.Title: {
+				return embed.setTitle(data[1]);
+			}
+			case TsxTypes.Description: {
+				return embed.setDescription(data[1]);
+			}
+			case TsxTypes.Field: {
+				return embed.addField(data[1], data[2], data[3]);
+			}
+			case TsxTypes.Footer: {
+				return embed.setFooter(data[1], data[2]);
+			}
+			case TsxTypes.Timestamp: {
+				return embed.setTimestamp(data[1]);
+			}
+			case TsxTypes.Url: {
+				return embed.setURL(data[1]);
+			}
+		}
+	}
+}
