f4e480d5c2c20b9839a294126d8eaaded99f5802
Gryffon Bellish <owenbellish@gmail.com>
2021-04-23 16:24:56 +0000

    feat: type all the elements

--- packages/embed-jsx/src/index.ts
+++ packages/embed-jsx/src/index.ts
@@ -1,1 +1,22 @@
+import type { MessageEmbed, MessageEmbedOptions } from 'discord.js';
+
 export { EmbedJsx } from './lib/EmbedJsx';
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace JSX {
+		interface IntrinsicElements {
+			embed: MessageEmbedOptions;
+			title: { url?: string };
+			description: Record<string, never>;
+			field: { name?: string; inline?: boolean };
+			timestamp: Record<string, never>;
+			footer: { iconURL?: string };
+			image: { url: string };
+			thumbnail: { url: string };
+			author: { url?: string; iconURL?: string };
+		}
+
+		type Element = MessageEmbed;
+	}
+}
