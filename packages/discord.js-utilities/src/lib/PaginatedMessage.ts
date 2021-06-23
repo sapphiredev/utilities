77771b75c7dc689fa554e07e3c68c39b09e3b14b
Kaname <56084970+kaname-png@users.noreply.github.com>
2021-06-23 13:08:01 -0600

    Resolve typecast
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>

--- packages/discord.js-utilities/src/lib/PaginatedMessage.ts
+++ packages/discord.js-utilities/src/lib/PaginatedMessage.ts
@@ -283,7 +283,7 @@ export class PaginatedMessage {
 	 * @param user The user that reacted to the message.
 	 */
 	protected async handleCollect(author: User, channel: TextChannel | NewsChannel, reaction: MessageReaction, user: User): Promise<void> {
-		if ((channel as TextChannel).permissionsFor(channel.client.user?.id || '')?.has('MANAGE_MESSAGES')) {
+		if (isGuildBasedChannel(channel) && channel.client.user && channel.permissionsFor(channel.client.user.id).has('MANAGE_MESSAGES')) {
 			await reaction.users.remove(user);
 		}
 
