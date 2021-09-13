2ec600236b0f9eb0a595aa2b0568a7baf0a5684b
UndiedGamer <84702365+UndiedGamer@users.noreply.github.com>
2021-09-13 15:37:52 +0530

    Update isNullOrUndefinedOrEmpty.ts

--- packages/utilities/src/lib/isNullOrUndefinedOrEmpty.ts
+++ packages/utilities/src/lib/isNullOrUndefinedOrEmpty.ts
@@ -6,5 +6,5 @@ import type { Nullish } from './utilityTypes';
  * @param value The value to check
  */
 export function isNullOrUndefinedOrEmpty(value: unknown): value is Nullish | '' {
-	return isNullOrUndefined(value) || Reflect.get(value, 'length') === 0 || value === '';
+	return isNullOrUndefined(value) || (value as string | unknown[]).length === 0;
 }
