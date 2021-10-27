1e6a3b36c39f04af2c2faff4f28523f5e2a32191
Jeroen Claassens <support@favware.tech>
2021-10-28 01:10:07 +0200

    fix(deepClone): fixed deep cloning when there are classes
    This was a bug that's been around since the Klasa days.  `isObject` returns `false` for classes.

--- packages/utilities/src/lib/deepClone.ts
+++ packages/utilities/src/lib/deepClone.ts
@@ -1,4 +1,3 @@
-import { isObject } from './isObject';
 import { isPrimitive } from './isPrimitive';
 
 /**
@@ -7,26 +6,57 @@ import { isPrimitive } from './isPrimitive';
  */
 export function deepClone<T>(source: T): T {
 	// Check if it's a primitive (with exception of function and null, which is typeof object)
-	if (source === null || isPrimitive(source)) return source;
+	if (source === null || isPrimitive(source)) {
+		return source;
+	}
+
 	if (Array.isArray(source)) {
-		const output = [] as unknown as T & T extends (infer S)[] ? S[] : never;
-		for (const value of source) output.push(deepClone(value));
+		const output = new (source.constructor as ArrayConstructor)(source.length) as unknown as T & T extends (infer S)[] ? S[] : never;
+
+		for (let i = 0; i < source.length; i++) {
+			output[i] = deepClone(source[i]);
+		}
+
 		return output as unknown as T;
 	}
-	if (isObject(source)) {
-		const output = {} as Record<PropertyKey, unknown>;
-		for (const [key, value] of Object.entries(source)) output[key] = deepClone(value);
-		return output as unknown as T;
-	}
+
 	if (source instanceof Map) {
 		const output = new (source.constructor as MapConstructor)() as unknown as T & T extends Map<infer K, infer V> ? Map<K, V> : never;
-		for (const [key, value] of source.entries()) output.set(key, deepClone(value));
+
+		for (const [key, value] of source.entries()) {
+			output.set(key, deepClone(value));
+		}
+
 		return output as unknown as T;
 	}
+
 	if (source instanceof Set) {
 		const output = new (source.constructor as SetConstructor)() as unknown as T & T extends Set<infer K> ? Set<K> : never;
-		for (const value of source.values()) output.add(deepClone(value));
+
+		for (const value of source.values()) {
+			output.add(deepClone(value));
+		}
+
 		return output as unknown as T;
 	}
+
+	if (typeof source === 'object') {
+		const output = new ((source as T & (object | Record<PropertyKey, unknown>)).constructor as ObjectConstructor)() as unknown as Record<
+			PropertyKey,
+			unknown
+		>;
+
+		for (const [key, value] of Object.entries(source)) {
+			Object.defineProperty(output, key, {
+				configurable: true,
+				enumerable: true,
+				value: deepClone(value),
+				writable: true
+			});
+		}
+
+		return output as unknown as T;
+	}
+
 	return source;
 }
