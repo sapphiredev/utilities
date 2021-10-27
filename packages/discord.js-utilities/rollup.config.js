cb7415f88925067632f800a88a09296b22203bdd
Jeroen Claassens <support@favware.tech>
2021-10-27 22:22:46 +0200

    fix(discord.js-utilities): build with tsc instead of rollup

--- packages/discord.js-utilities/rollup.config.js
+++
@@ -1,7 +0,0 @@
-import baseConfig from '../../scripts/rollup.config';
-
-export default baseConfig({
-	extraOptions: {
-		external: ['@sapphire/discord-utilities', '@sapphire/time-utilities', '@sapphire/utilities', 'discord.js']
-	}
-});
