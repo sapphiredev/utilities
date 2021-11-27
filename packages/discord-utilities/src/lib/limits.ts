de71f0a2336c986cfb16de2b5808ee52579c3e72
Elliot Maisl <elliot.maisl@gmail.com>
2021-11-27 21:40:14 +0100

    refactor: namespace + max => maximum

--- packages/discord-utilities/src/lib/limits.ts
+++ packages/discord-utilities/src/lib/limits.ts
@@ -1,99 +1,112 @@
-/**
- * Maximum characters allowed in a channel description.
- */
-export const MaxChannelDescriptionLength = 1024;
+export const ChannelLimits = {
+	/**
+	 * Maximum characters allowed in a channel description.
+	 */
+	MaximumDescriptionLength: 1024,
+	/**
+	 * Maximum characters allowed in a channel name.
+	 */
+	MaximumNameLength: 100,
+	/**
+	 * Maximum viewers allowed per screen share.
+	 */
+	MaximumViewersPerScreenShare: 250
+};
 
-/**
- * Maximum characters allowed in a channel name.
- */
-export const MaxChannelNameLength = 100;
+export const EmbedLimits = {
+	/**
+	 * Maximum characters allowed in the author field of an embed.
+	 */
+	MaximumAuthorNameLength: 256,
 
-/**
- * Maximum channels allowed per guild, including category channels.
- */
-export const MaxChannelsPerGuild = 500;
+	/**
+	 * Maximum characters allowed in an embed description.
+	 */
+	MaximumDescriptionLength: 4096,
 
-/**
- * Maximum characters allowed in the display name of a guild member.
- */
-export const MaxDisplayNameLength = 32;
+	/**
+	 * Maximum characters allowed in the name of a field in an embed.
+	 */
+	MaximumFieldNameLength: 256,
 
-/**
- * Maximum characters allowed in the author field of an embed.
- */
-export const MaxEmbedAuthorNameLength = 256;
+	/**
+	 * Maximum fields allowed in an embed.
+	 */
+	MaximumFields: 25,
 
-/**
- * Maximum characters allowed in an embed description.
- */
-export const MaxEmbedDescriptionLength = 4096;
+	/**
+	 * Maximum characters allowed in the avlue of a field in an embed.
+	 */
+	MaximumFieldValueLength: 1024,
 
-/**
- * Maximum characters allowed in the name of a field in an embed.
- */
-export const MaxEmbedFieldNameLength = 256;
+	/**
+	 * Maximum characters allowed in a footer of an embed.
+	 */
+	MaximumFooterLength: 2048,
 
-/**
- * Maximum fields allowed in an embed.
- */
-export const MaxEmbedFields = 25;
+	/**
+	 * Maximum characters allowed in the title of an embed.
+	 */
+	MaximumTitleLength: 256,
 
-/**
- * Maximum characters allowed in the avlue of a field in an embed.
- */
-export const MaxEmbedFieldValueLength = 1024;
+	/**
+	 * Maximum characters allowed in an embed, in total.
+	 */
+	MaximumTotalCharacters: 6000
+};
 
-/**
- * Maximum characters allowed in a footer of an embed.
- */
-export const MaxEmbedFooterLength = 2048;
+export const EmojiLimits = {
+	/**
+	 * Maximum characters allowed in a custom guild emoji.
+	 */
+	MaximumEmojiNameLength: 32
+};
 
-/**
- * Maximum embeds allowed in a single message.
- */
-export const MaxEmbedsPerMessage = 10;
+export const GuildLimits = {
+	/**
+	 * Maximum channels allowed per guild, including category channels.
+	 */
+	MaximumChannels: 500,
+	/**
+	 * Maximum roles allowed in a guild.
+	 */
+	MaximumRoles: 250
+};
 
-/**
- * Maximum characters allowed in the title of an embed.
- */
-export const MaxEmbedTitleLength = 256;
+export const GuildMemberLimits = {
+	/**
+	 * Maximum characters allowed in the display name of a guild member.
+	 */
+	MaxDisplayNameLength: 32
+};
 
-/**
- * Maximum characters allowed in an embed, in total.
- */
-export const MaxEmbedTotalCharacters = 6000;
+export const MessageLimits = {
+	/**
+	 * Maximum embeds allowed in a single message.
+	 */
+	MaximumEmbeds: 10,
 
-/**
- * Maximum characters allowed in a custom guild emoji.
- */
-export const MaxEmojiNameLength = 32;
+	/**
+	 * Maximum characters allowed in a single message for a user.
+	 */
+	MaximumLength: 2000,
 
-/**
- * Maximum characters allowed in a single message for a user.
- */
-export const MaxMessageLength = 2000;
+	/**
+	 * Maximum characters allowed in a single message for a nitro user.
+	 */
+	MaximumNitroLength: 4000
+};
 
-/**
- * Maximum characters allowed in a single message for a nitro user.
- */
-export const MaxNitroMessageLength = 4000;
+export const RoleLimits = {
+	/**
+	 * Maximum characters allowed in a role name.
+	 */
+	MaximumNameLength: 100
+};
 
-/**
- * Maximum characters allowed in a role name.
- */
-export const MaxRoleNameLength = 100;
-
-/**
- * Maximum roles allowed in a guild.
- */
-export const MaxRolesPerGuild = 250;
-
-/**
- * Maximum viewers allowed per screen share.
- */
-export const MaxUsersPerDMGroup = 10;
-
-/**
- * Maximum viewers allowed per screen share.
- */
-export const MaxViewersPerScreenShare = 250;
+export const UserLimits = {
+	/**
+	 * Maximum viewers allowed per screen share.
+	 */
+	MaximumUsersPerDMGroup: 10
+};
