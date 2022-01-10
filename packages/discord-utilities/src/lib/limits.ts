38410d2305d55e47bded9795d8e99a92304aea1a
Elliot <elliot.maisl@gmail.com>
2022-01-11 00:41:31 +0100

    feat(discord-utilities): add more limits (#253)
    Co-authored-by: Vlad Frangu <kingdgrizzle@gmail.com>
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>

--- packages/discord-utilities/src/lib/limits.ts
+++ packages/discord-utilities/src/lib/limits.ts
@@ -6,10 +6,12 @@ export const ChannelLimits = {
 	 * Maximum characters allowed in a channel description.
 	 */
 	MaximumDescriptionLength: 1024,
+
 	/**
 	 * Maximum characters allowed in a channel name.
 	 */
 	MaximumNameLength: 100,
+
 	/**
 	 * Maximum viewers allowed per screen share.
 	 */
@@ -79,6 +81,7 @@ export const GuildLimits = {
 	 * Maximum channels allowed per guild, including category channels.
 	 */
 	MaximumChannels: 500,
+
 	/**
 	 * Maximum roles allowed in a guild.
 	 */
@@ -137,7 +140,40 @@ export const MessageLimits = {
 	/**
 	 * Maximum characters allowed in a single message for a nitro user.
 	 */
-	MaximumNitroLength: 4000
+	MaximumNitroLength: 4000,
+
+	/**
+	 * Maximum numbers of reactions allowed for a message.
+	 */
+	MaximumReactions: 20,
+
+	/**
+	 * Maximum upload size for a free user in a guild of tier 1 or below, or in DMs.
+	 * Size is in bytes, and correspond to 8MB.
+	 */
+	MaximumUploadSize: 8_000_000,
+
+	/**
+	 * Maximum upload size for a nitro user, in any guild or in DMs.
+	 * Size is in bytes, and correspond to 10MB.
+	 */
+	MaximumNitroUploadSize: 100_000_000,
+
+	/**
+	 * Maximum upload size for a free user for all different boost levels available in a guild.
+	 * Sizes are in bytes, and correspond to 8MB, 8MB, 50MB, and 100MB.
+	 */
+	MaximumUploadSizeInGuild: [8_000_000, 8_000_000, 50_000_000, 100_000_000]
+} as const;
+
+/**
+ * Namespace containing limits related to built-in moderation features.
+ */
+export const ModerationLimits = {
+	/**
+	 * Maximum duration of a guild timeout, in seconds (corresponds to 28 days).
+	 */
+	MaximumTimeoutDuration: 2_419_200
 } as const;
 
 /**
@@ -157,5 +193,10 @@ export const UserLimits = {
 	/**
 	 * Maximum numbers of users in a DM group.
 	 */
-	MaximumUsersPerDMGroup: 10
+	MaximumUsersPerDMGroup: 10,
+
+	/**
+	 * Maximum characters allowed in a user's biography (the "About Me" section).
+	 */
+	MaximumBiographyLength: 190
 } as const;
