8ba1b7e5a2016da3510a0c3daae0ef3dec54281b
Kaname <56084970+kaname-png@users.noreply.github.com>
2022-01-15 16:28:37 -0600

    fix: export class PaginatedMessageEmbedFields

--- packages/discord.js-utilities/src/lib/PaginatedMessages/index.ts
+++ packages/discord.js-utilities/src/lib/PaginatedMessages/index.ts
@@ -2,4 +2,5 @@ export * from './LazyPaginatedMessage';
 export * from './PaginatedFieldMessageEmbed';
 export * from './PaginatedMessage';
 export * from './PaginatedMessageTypes';
+export * from './PaginatedMessageEmbedFields';
 export * from './utils';
