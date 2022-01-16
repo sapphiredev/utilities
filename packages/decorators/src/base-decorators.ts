38e2977b8dc061d0d62f076b12808b196e92b73a
Jeroen Claassens <support@favware.tech>
2022-01-16 20:51:42 +0100

    feat(decorators): remove `enumerable` and `enumerableMethod`. Use their PascalCased variants instead.
    BREAKING CHANGE: The previously deprecated enumerable` and
    `enumerableMethod` decorators have been removed. Use their PascalCased variants instead.

--- packages/decorators/src/base-decorators.ts
+++ packages/decorators/src/base-decorators.ts
@@ -22,14 +22,6 @@ export function Enumerable(value: boolean) {
 }
 
 /**
- * Decorator that sets the enumerable property of a class field to the desired value.
- * @param value Whether the property should be enumerable or not
- * @deprecated Use `@Enumerable` instead.
- * The lowercased decorator will be removed in the next major version of `@sapphire/decorators`
- */
-export const enumerable = Enumerable;
-
-/**
  * Decorator that sets the enumerable property of a class method to the desired value.
  * @param value Whether the method should be enumerable or not
  */
@@ -38,11 +30,3 @@ export function EnumerableMethod(value: boolean) {
 		descriptor.enumerable = value;
 	});
 }
-
-/**
- * Decorator that sets the enumerable property of a class method to the desired value.
- * @param value Whether the method should be enumerable or not
- * @deprecated Use `@EnumerableMethod` instead.
- * The lowercased decorator will be removed in the next major version of `@sapphire/decorators`
- */
-export const enumerableMethod = EnumerableMethod;
