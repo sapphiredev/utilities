aea6a83d3d7116a15dbfd4ebc27029d8b637a0f2
Jeroen Claassens <support@favware.tech>
2022-01-24 00:07:24 +0100

    feat(package): add new package @sapphire/result

---
+++ packages/result/tests/Maybe.test.ts
@@ -0,0 +1,23 @@
+import { maybe, isSome, isNone, some, none, isMaybe } from '../src/index';
+
+describe('Maybe', () => {
+	test('some', () => {
+		const returnValue = some(42);
+
+		expect(isSome(returnValue)).toBeTruthy();
+		expect(isNone(returnValue)).toBeFalsy();
+	});
+
+	test('none', () => {
+		const returnValue = none();
+
+		expect(isSome(returnValue)).toBeFalsy();
+		expect(isNone(returnValue)).toBeTruthy();
+	});
+
+	test('maybe', () => {
+		const returnValue = maybe(42);
+
+		expect(isMaybe(returnValue)).toBeTruthy();
+	});
+});
