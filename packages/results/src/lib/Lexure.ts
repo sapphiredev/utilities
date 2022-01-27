a5aa2b2f9b69c72f5bfd5e97d576c70a6aff57e5
c43721 <rlahman.game@gmail.com>
2022-01-26 18:40:54 -0600

    feat: port types from framework and lexure

---
+++ packages/results/src/lib/Lexure.ts
@@ -0,0 +1,77 @@
+/**
+ * A type used to express computations that can fail.
+ * @typeparam T - Type of results.
+ * @typeparam E - Type of errors.
+ */
+ export type Result<T, E> = Ok<T> | Err<E>;
+
+ /**
+  * The computation is successful.
+  * @typeparam T - Type of results.
+  */
+ export interface Ok<T> {
+	 /**
+	  * If this is an Ok, this is true.
+	  */
+	 readonly success: true;
+ 
+	 /**
+	  * The resulting value, which only exists on an Ok.
+	  */
+	 readonly value: T;
+ 
+	 readonly error?: undefined
+ }
+ 
+ /**
+  * The computation failed.
+  * @typeparam E - Type of errors.
+  */
+ export interface Err<E> {
+	 /**
+	  * If this an Err, this is false.
+	  */
+	 readonly success: false;
+ 
+	 readonly value?: undefined
+ 
+	 /**
+	  * The resulting error, which only exists on an Err.
+	  */
+	 readonly error: E;
+ }
+
+/**
+ * A type that can express the lack of a value.
+ * Used in this library for when a generic type could be nullable.
+ * @typeparam T - Type of results.
+ */
+ export type Option<T> = Some<T> | None;
+
+ /**
+  * The value exists.
+  * @typeparam T - Type of results.
+  */
+ export interface Some<T> {
+	 /**
+	  * If this is a Some, this is true.
+	  */
+	 readonly exists: true;
+ 
+	 /**
+	  * The value, which only exists on a Some.
+	  */
+	 readonly value: T;
+ }
+ 
+ /**
+  * The value does not exist.
+  */
+ export interface None {
+	 /**
+	  * If this is a None, this is false.
+	  */
+	 readonly exists: false;
+ 
+	 readonly value?: undefined;
+ }
