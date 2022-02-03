7a15a2f47f3d7ff7828859f0dfe7d8281726cdc7
Jeroen Claassens <support@favware.tech>
2022-02-03 20:16:04 +0100

    fix(discord.js-utilities): fixed `PaginatedMessage` not filtering to the correct response and user

--- packages/discord.js-utilities/src/index.ts
+++ packages/discord.js-utilities/src/index.ts
@@ -6,4 +6,4 @@ export * from './lib/type-guards';
 export * from './lib/utilities';
 export * from './lib/utility-types';
 
-console.log('This is just to have this commit show up in the changelog');
+console.log('This is just to have this commit show up in the changelog and once more');
