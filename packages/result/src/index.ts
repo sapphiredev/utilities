ba3c5b9bc2969683c83116dbbdafda2a049a2264
c43721 <rlahman.game@gmail.com>
2022-02-05 17:29:25 -0600

    fix: jesus christ

--- packages/result/src/index.ts
+++ packages/result/src/index.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export * from "./lib/Lexure";
-export * from "./lib/Maybe";
-export * from "./lib/Result";
-=======
 export * from './lib/Lexure';
 export * from './lib/Maybe';
 export * from './lib/Result';
->>>>>>> b77b5ebe00bbbed2da1e5258708c642cf535a0d9
