b0e1589254392b05b29103d182321516d2b10f49
c43721 <rlahman.game@gmail.com>
2022-02-05 17:35:11 -0600

    feat(test): add parser tests

---
+++ packages/result/tests/Parsers.test.ts
@@ -0,0 +1,31 @@
+import { from, fromAsync, isErr, isOk } from '../src/index';
+
+describe('Parsers', () => {
+	test('from - ok', () => {
+		const returnValue = from(() => 42);
+
+		expect(isOk(returnValue)).toBeTruthy();
+		expect(isErr(returnValue)).toBeFalsy();
+	});
+
+	test('from - error', () => {
+		const returnValue = from(() => new Error());
+
+		expect(isOk(returnValue)).toBeFalsy();
+		expect(isErr(returnValue)).toBeTruthy();
+	});
+
+	test('fromAsync - ok', async () => {
+		const returnValue = await fromAsync(() => Promise.resolve(42));
+
+		expect(isOk(returnValue)).toBeTruthy();
+		expect(isErr(returnValue)).toBeFalsy();
+	});
+
+	test('fromAsync - error', async () => {
+		const returnValue = await fromAsync(() => Promise.reject(new Error()));
+
+		expect(isOk(returnValue)).toBeFalsy();
+		expect(isErr(returnValue)).toBeTruthy();
+	});
+});
