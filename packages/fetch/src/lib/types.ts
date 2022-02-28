9f79327380bea6afef009a618f18740902a8005b
kaname-png <inmortaldragonxspace@outlook.com>
2022-02-28 13:05:29 -0600

    feat(fetch): content types alphabetically sorted

--- packages/fetch/src/lib/types.ts
+++ packages/fetch/src/lib/types.ts
@@ -88,119 +88,10 @@ export const enum FetchMethods {
  */
 export const enum FetchMediaContentTypes {
 	/**
-	 * The `application/json` media content type.
-	 * @see [Media-Types / application / json](https://www.iana.org/assignments/media-types/application/json)
-	 * @see [[RFC8259](https://www.iana.org/go/rfc8259)]
-	 */
-	JSON = 'application/json',
-	/**
-	 * The `application/xml` media content type.
-	 * @see [Media-Types / application / xml](https://www.iana.org/assignments/media-types/application/xml)
-	 * @see [[RFC7303](https://www.iana.org/go/rfc7303)]
-	 */
-	XML = 'application/xml',
-	/**
-	 * The `x-www-form-urlencoded` media content type.
-	 * @see [Media-Types / application / x-www-form-urlencoded](https://www.iana.org/assignments/media-types/application/x-www-form-urlencoded)
-	 */
-	FormURLEncoded = 'application/x-www-form-urlencoded',
-	/**
-	 * The `application/octet-stream` media content type.
-	 * @see [Media-Types / application / octet-stream](https://www.iana.org/assignments/media-types/application/octet-stream)
-	 * @see [[RFC2045](https://www.iana.org/go/rfc2045)] [[RFC2046(https://www.iana.org/go/rfc2046)]
-	 */
-	OctetStream = 'application/octet-stream',
-	/**
-	 * The `application/javascript` media content type.
-	 * @see [Media-Types / application / javascript](https://www.iana.org/assignments/media-types/application/javascript)
-	 * @see [[RFC4329](https://www.iana.org/go/rfc4329)]
-	 */
-	JavaScript = 'application/javascript',
-	/**
-	 * The `multipart/form-data` media content type.
-	 * @see [Media-Types / multipart / form-data](https://www.iana.org/assignments/media-types/multipart/form-data)
-	 * @see [[RFC7578](https://www.iana.org/go/rfc7578)]
-	 */
-	FormData = 'multipart/form-data',
-	/**
-	 * The `text/HTML` media content type.
-	 * @see [Media-Types / text / html](https://www.iana.org/assignments/media-types/text/html)
-	 */
-	TextHTML = 'text/html',
-	/**
-	 * The `text/css` media content type.
-	 * @see [Media-Types / text / css](https://www.iana.org/assignments/media-types/text/css)
-	 * @see [[RFC2318](https://www.iana.org/go/rfc2318)]
-	 */
-	TextCSS = 'text/css',
-	/**
-	 * The `text/plain` media content type.
-	 * @see [Media-Types / text / plain](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#textplain)
-	 * @see [[RFC2046](https://www.iana.org/go/rfc2046)] [[RFC3676](https://www.iana.org/go/rfc3676)] [[RFC5147](https://www.iana.org/go/rfc5147)]
-	 */
-	TextPlain = 'text/plain',
-	/**
-	 * The `image/gif` media content type.
-	 * @see [Media-Types / image / gif](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#gif_graphics_interchange_format)
-	 * @see [[RFC2045](https://www.iana.org/go/rfc2045)] [[RFC2046](https://www.iana.org/go/rfc2046)]
-	 */
-	ImageGIF = 'image/gif',
-	/**
-	 * The `image/jpeg` media content type.
-	 * @see [Media-Types / image / jpeg](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#jpeg_joint_photographic_experts_group_image)
-	 * @see [[RFC2045](https://www.iana.org/go/rfc2045)] [[RFC2046](https://www.iana.org/go/rfc2046)]
-	 */
-	ImageJPEG = 'image/jpeg',
-	/**
-	 * The `image/png` media content type.
-	 * @see [Media-Types / image / png](https://www.iana.org/assignments/media-types/image/png)
-	 */
-	ImagePNG = 'image/png',
-	/**
-	 * The `image/webp` media content type.
-	 * @see [Media-Types / image / webp](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#webp_image)
-	 */
-	ImageWEBP = 'image/webp',
-	/**
-	 * The `image/apng` media content type.
-	 * @see [Media-Types / image / apng](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#apng_animated_portable_network_graphics)
-	 */
-	ImageAPNG = 'image/apng',
-	/**
-	 * The `audio/webm` media content type.
-	 */
-	AudioWebm = 'audio/webm',
-	/**
-	 * The `video/webm` media content type.
-	 */
-	VideoWebm = 'video/webm',
-	/**
-	 * The `audio/ogg` media content type.
-	 * @see [Media-Types / audio / ogg](https://www.iana.org/assignments/media-types/audio/ogg)
-	 * @see [[RFC5334](https://www.iana.org/go/rfc5334)] [[RFC7845](https://www.iana.org/go/rfc7845)]
-	 */
-	AudioOgg = 'audio/ogg',
-	/**
-	 * The `video/ogg` media content type.
-	 * @see [Media-Types / video / ogg](https://www.iana.org/assignments/media-types/video/ogg)
-	 * @see [[RFC5334](https://www.iana.org/go/rfc5334)] [[RFC7845](https://www.iana.org/go/rfc7845)]
-	 */
-	VideoOgg = 'video/ogg',
-	/**
 	 * The `audio/aac` media content type.
 	 * @see [Media-Types / audio / aac](https://www.iana.org/assignments/media-types/audio/aac)
 	 */
 	AudioAac = 'audio/aac',
-	/**
-	 * The `audio/wav` media content type.
-	 */
-	AudioWav = 'audio/wav',
-	/**
-	 * The `audio/opus` media content type.
-	 * @see [Media-Types / audio / opus](https://www.iana.org/assignments/media-types/audio/opus)
-	 * @see [[RFC7587](https://www.iana.org/go/rfc7587)]
-	 */
-	AudioOpus = 'audio/opus',
 	/**
 	 * The `audio/mp4` media content type.
 	 * @see [Media-Types / audio / mp4](https://www.iana.org/assignments/media-types/audio/mp4)
@@ -214,29 +105,31 @@ export const enum FetchMediaContentTypes {
 	 */
 	AudioMpeg = 'audio/mpeg',
 	/**
+	 * The `audio/ogg` media content type.
+	 * @see [Media-Types / audio / ogg](https://www.iana.org/assignments/media-types/audio/ogg)
+	 * @see [[RFC5334](https://www.iana.org/go/rfc5334)] [[RFC7845](https://www.iana.org/go/rfc7845)]
+	 */
+	AudioOgg = 'audio/ogg',
+	/**
+	 * The `audio/opus` media content type.
+	 * @see [Media-Types / audio / opus](https://www.iana.org/assignments/media-types/audio/opus)
+	 * @see [[RFC7587](https://www.iana.org/go/rfc7587)]
+	 */
+	AudioOpus = 'audio/opus',
+	/**
 	 * The `audio/vorbis` media content type.
 	 * @see [Media-Types / audio / vorbis](https://www.iana.org/assignments/media-types/audio/vorbis)
 	 * @see [[RFC5215](https://www.iana.org/go/rfc5215)]
 	 */
 	AudioVorbis = 'audio/vorbis',
 	/**
-	 * The `video/h264` media content type.
-	 * @see [Media-Types / video / h264](https://www.iana.org/assignments/media-types/video/H264)
-	 * @see [[RFC6184](https://www.iana.org/go/rfc6184)]
+	 * The `audio/wav` media content type.
 	 */
-	VideoH264 = 'video/h264',
+	AudioWav = 'audio/wav',
 	/**
-	 * The `video/h265` media content type.
-	 * @see [Media-Types / video / h265](https://www.iana.org/assignments/media-types/video/H265)
-	 * @see [[RFC7798](https://www.iana.org/go/rfc7798)]
+	 * The `audio/webm` media content type.
 	 */
-	VideoH265 = 'video/h265',
-	/**
-	 * The `video/mp4` media content type.
-	 * @see [Media-Types / video / mp4](https://www.iana.org/assignments/media-types/video/mp4)
-	 * @see [[RFC4337](https://www.iana.org/go/rfc4337)] [[RFC6381](https://www.iana.org/go/rfc6381)]
-	 */
-	VideoMp4 = 'video/mp4',
+	AudioWebm = 'audio/webm',
 	/**
 	 * The `font/otf` media content type.
 	 * @see [Media-Types / font / otf](https://www.iana.org/assignments/media-types/font/otf)
@@ -260,5 +153,112 @@ export const enum FetchMediaContentTypes {
 	 * @see [Media-Types / font / woff2](https://www.iana.org/assignments/media-types/font/woff2)
 	 * @see [[RFC8081](https://www.iana.org/go/rfc8081)]
 	 */
-	FontWoff2 = 'font/woff2'
+	FontWoff2 = 'font/woff2',
+	/**
+	 * The `multipart/form-data` media content type.
+	 * @see [Media-Types / multipart / form-data](https://www.iana.org/assignments/media-types/multipart/form-data)
+	 * @see [[RFC7578](https://www.iana.org/go/rfc7578)]
+	 */
+	FormData = 'multipart/form-data',
+	/**
+	 * The `x-www-form-urlencoded` media content type.
+	 * @see [Media-Types / application / x-www-form-urlencoded](https://www.iana.org/assignments/media-types/application/x-www-form-urlencoded)
+	 */
+	FormURLEncoded = 'application/x-www-form-urlencoded',
+	/**
+	 * The `image/apng` media content type.
+	 * @see [Media-Types / image / apng](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#apng_animated_portable_network_graphics)
+	 */
+	ImageAPNG = 'image/apng',
+	/**
+	 * The `image/gif` media content type.
+	 * @see [Media-Types / image / gif](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#gif_graphics_interchange_format)
+	 * @see [[RFC2045](https://www.iana.org/go/rfc2045)] [[RFC2046](https://www.iana.org/go/rfc2046)]
+	 */
+	ImageGIF = 'image/gif',
+	/**
+	 * The `image/jpeg` media content type.
+	 * @see [Media-Types / image / jpeg](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#jpeg_joint_photographic_experts_group_image)
+	 * @see [[RFC2045](https://www.iana.org/go/rfc2045)] [[RFC2046](https://www.iana.org/go/rfc2046)]
+	 */
+	ImageJPEG = 'image/jpeg',
+	/**
+	 * The `image/png` media content type.
+	 * @see [Media-Types / image / png](https://www.iana.org/assignments/media-types/image/png)
+	 */
+	ImagePNG = 'image/png',
+	/**
+	 * The `image/webp` media content type.
+	 * @see [Media-Types / image / webp](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types#webp_image)
+	 */
+	ImageWEBP = 'image/webp',
+	/**
+	 * The `application/json` media content type.
+	 * @see [Media-Types / application / json](https://www.iana.org/assignments/media-types/application/json)
+	 * @see [[RFC8259](https://www.iana.org/go/rfc8259)]
+	 */
+	JSON = 'application/json',
+	/**
+	 * The `application/javascript` media content type.
+	 * @see [Media-Types / application / javascript](https://www.iana.org/assignments/media-types/application/javascript)
+	 * @see [[RFC4329](https://www.iana.org/go/rfc4329)]
+	 */
+	JavaScript = 'application/javascript',
+	/**
+	 * The `application/octet-stream` media content type.
+	 * @see [Media-Types / application / octet-stream](https://www.iana.org/assignments/media-types/application/octet-stream)
+	 * @see [[RFC2045](https://www.iana.org/go/rfc2045)] [[RFC2046(https://www.iana.org/go/rfc2046)]
+	 */
+	OctetStream = 'application/octet-stream',
+	/**
+	 * The `text/css` media content type.
+	 * @see [Media-Types / text / css](https://www.iana.org/assignments/media-types/text/css)
+	 * @see [[RFC2318](https://www.iana.org/go/rfc2318)]
+	 */
+	TextCSS = 'text/css',
+	/**
+	 * The `text/HTML` media content type.
+	 * @see [Media-Types / text / html](https://www.iana.org/assignments/media-types/text/html)
+	 */
+	TextHTML = 'text/html',
+	/**
+	 * The `text/plain` media content type.
+	 * @see [Media-Types / text / plain](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#textplain)
+	 * @see [[RFC2046](https://www.iana.org/go/rfc2046)] [[RFC3676](https://www.iana.org/go/rfc3676)] [[RFC5147](https://www.iana.org/go/rfc5147)]
+	 */
+	TextPlain = 'text/plain',
+	/**
+	 * The `video/h264` media content type.
+	 * @see [Media-Types / video / h264](https://www.iana.org/assignments/media-types/video/H264)
+	 * @see [[RFC6184](https://www.iana.org/go/rfc6184)]
+	 */
+	VideoH264 = 'video/h264',
+	/**
+	 * The `video/h265` media content type.
+	 * @see [Media-Types / video / h265](https://www.iana.org/assignments/media-types/video/H265)
+	 * @see [[RFC7798](https://www.iana.org/go/rfc7798)]
+	 */
+	VideoH265 = 'video/h265',
+	/**
+	 * The `video/mp4` media content type.
+	 * @see [Media-Types / video / mp4](https://www.iana.org/assignments/media-types/video/mp4)
+	 * @see [[RFC4337](https://www.iana.org/go/rfc4337)] [[RFC6381](https://www.iana.org/go/rfc6381)]
+	 */
+	VideoMp4 = 'video/mp4',
+	/**
+	 * The `video/ogg` media content type.
+	 * @see [Media-Types / video / ogg](https://www.iana.org/assignments/media-types/video/ogg)
+	 * @see [[RFC5334](https://www.iana.org/go/rfc5334)] [[RFC7845](https://www.iana.org/go/rfc7845)]
+	 */
+	VideoOgg = 'video/ogg',
+	/**
+	 * The `video/webm` media content type.
+	 */
+	VideoWebm = 'video/webm',
+	/**
+	 * The `application/xml` media content type.
+	 * @see [Media-Types / application / xml](https://www.iana.org/assignments/media-types/application/xml)
+	 * @see [[RFC7303](https://www.iana.org/go/rfc7303)]
+	 */
+	XML = 'application/xml'
 }
