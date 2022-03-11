d0146d9ac68934b41dcb5bd7d057b532a507f99f
Jeroen Claassens <support@favware.tech>
2022-03-11 11:06:18 +0100

    chore: refactored configuration.
    permalink: http://whatthecommit.com/9b0ccc2a6b0ce88b902c6be0cb6f0c0a

--- packages/utilities/src/lib/toTitleCase.ts
+++ packages/utilities/src/lib/toTitleCase.ts
@@ -1,5 +1,18 @@
 const TO_TITLE_CASE = /[A-Za-zÀ-ÖØ-öø-ÿ]\S*/g;
-const baseVariants: Record<string, string> = {
+
+/**
+ * The variants that will not strictly follow the `toTitleCase` algorithm and will instead return the value matched with the key.
+ *
+ * This table lists how certain terms are converted, these are case insensitive.
+ * Any terms not included are converted to regular `Titlecase`.
+ * |       Term      	|   Converted To  	|
+ * |:---------------:	|:---------------:	|
+ * | textchannel     	| TextChannel     	|
+ * | voicechannel    	| VoiceChannel    	|
+ * | categorychannel 	| CategoryChannel 	|
+ * | guildmember     	| GuildMember     	|
+ */
+export const baseVariants: Record<string, string> = {
 	textchannel: 'TextChannel',
 	voicechannel: 'VoiceChannel',
 	categorychannel: 'CategoryChannel',
@@ -8,25 +21,18 @@ const baseVariants: Record<string, string> = {
 
 /**
  * Converts a string to Title Case
- * @description This is designed to also ensure common Discord PascalCased strings
- * 				are put in their TitleCase baseVariants. See below for the full list.
- *              You can also provide your own variants to merge with the baseVariants
- *              for your own functionality use.
- * @param str The string to title case
- * @param variants The optional title case variants to merge with baseVariants
- * @terms
- * This table lists how certain terms are converted, these are case insensitive.
- * Any terms not included are converted to regular Titlecase.
  *
- *      | Term            |    Converted To |
- *      |-----------------|-----------------|
- *      | textchannel     |     TextChannel |
- *      | voicechannel    |    VoiceChannel |
- *      | categorychannel | CategoryChannel |
- *      | guildmember     |     GuildMember |
+ * @description This is designed to also ensure common Discord PascalCased strings
+ * are put in their TitleCase {@link baseVariants}.
+ *
+ * You can also provide your own variants to merge with the {@link baseVariants} for
+ * your own functionality use.
+ *
+ * @param str The string to title case
+ * @param additionalVariants The optional title case variants to merge with {@link baseVariants}
  */
-export function toTitleCase(str: string, variants: Record<string, string> = {}): string {
-	const titleCaseVariants = { ...baseVariants, ...variants };
+export function toTitleCase(str: string, additionalVariants: Record<string, string> = {}): string {
+	const titleCaseVariants = { ...baseVariants, ...additionalVariants };
 
 	return str.replace(TO_TITLE_CASE, (txt) => titleCaseVariants[txt] ?? txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase());
 }
