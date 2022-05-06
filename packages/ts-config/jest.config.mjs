bb430fa284723bacb4e4f7bfafff4a4dc6d56f8b
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-05-07 01:37:52 +0530

    chore: bump jest (#344)

--- packages/ts-config/jest.config.mjs
+++ packages/ts-config/jest.config.mjs
@@ -2,8 +2,6 @@
 const config = {
 	displayName: 'ts-jest',
 	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testRunner: 'jest-circus/runner',
 	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 	globals: {
 		'ts-jest': {
