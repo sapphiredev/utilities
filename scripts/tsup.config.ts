7d6e7f545ccc80e0ec08c82749179e5db60d9b45
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-05-15 19:32:25 +0530

    fix: simplify tsup config

--- scripts/tsup.config.ts
+++ scripts/tsup.config.ts
@@ -1,20 +1,6 @@
 import { relative, resolve as resolveDir } from 'node:path';
 import { defineConfig, type Options } from 'tsup';
 
-const defaultConfigOptions: ConfigOptions = {
-	globalName: undefined,
-	format: ['esm', 'cjs', 'iife'],
-	target: 'es2021',
-	sourcemap: true,
-	esbuildOptions: (options, context) => {
-		if (context.format === 'cjs') {
-			options.banner = {
-				js: '"use strict";'
-			};
-		}
-	}
-};
-
 export const createTsupConfig = ({
 	globalName = undefined,
 	format = ['esm', 'cjs', 'iife'],
@@ -27,7 +13,7 @@ export const createTsupConfig = ({
 			};
 		}
 	}
-}: ConfigOptions = defaultConfigOptions) =>
+}: ConfigOptions = {}) =>
 	defineConfig({
 		clean: true,
 		dts: false,
