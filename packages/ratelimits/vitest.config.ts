5f5220412746c2afbc980e0367d7c157d1c48d96
parbez <imranbarbhuiya.fsd@gmail.com>
2022-06-20 13:17:33 +0530

    test: all config setup

---
+++ packages/ratelimits/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		globals: true,
+		coverage: {
+			enabled: true,
+			reporter: ['text', 'lcov', 'clover']
+		}
+	},
+	esbuild: {
+		target: 'es2020'
+	}
+});
