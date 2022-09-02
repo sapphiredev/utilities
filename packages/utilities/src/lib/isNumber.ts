01e1b1428cf793d7716d60217204626c76bd872e
Kaname <56084970+kaname-png@users.noreply.github.com>
2022-09-02 13:33:37 -0600

    fix(isNumber): parse string parameter as number (allow string as parameter) (#444)
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>

--- packages/utilities/src/lib/isNumber.ts
+++ packages/utilities/src/lib/isNumber.ts
@@ -3,5 +3,6 @@
  * @param input The number to verify
  */
 export function isNumber(input: unknown): input is number {
-	return typeof input === 'number' && !isNaN(input) && Number.isFinite(input);
+	if (typeof input === 'string') input = Number(input);
+	return typeof input === 'number' && !Number.isNaN(input) && Number.isFinite(input);
 }
