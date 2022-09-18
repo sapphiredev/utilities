a6e20f0592c87fd2abe8e9130fef0b8089e1ca86
Jeroen Claassens <support@favware.tech>
2022-09-18 19:10:57 +0200

    fix(time-utilities): use type-only exports for interfaces

--- packages/time-utilities/src/index.ts
+++ packages/time-utilities/src/index.ts
@@ -3,12 +3,12 @@ export {
 	DEFAULT_SEPARATORS,
 	DEFAULT_UNITS,
 	Duration,
-	DurationFormatAssetsTime,
-	DurationFormatAssetsUnit,
-	DurationFormatSeparators,
 	DurationFormatter,
 	Time,
-	TimeTypes
+	TimeTypes,
+	type DurationFormatAssetsTime,
+	type DurationFormatAssetsUnit,
+	type DurationFormatSeparators
 } from '@sapphire/duration';
 export { TimerManager } from '@sapphire/timer-manager';
-export { days, months, TimeResolvable, Timestamp, TimestampTemplateEntry, tokens } from '@sapphire/timestamp';
+export { days, months, Timestamp, tokens, type TimeResolvable, type TimestampTemplateEntry } from '@sapphire/timestamp';
