6f4fafbe3e3ad485a37653ee406c6ed0b2d44e6d
parbez <imranbarbhuiya.fsd@gmail.com>
2022-09-29 23:45:44 +0530

    feat: add `findFilesRecursively`

---
+++ packages/utilities/src/lib/findFilesRecursively.ts
@@ -0,0 +1,97 @@
+import type { PathLike } from 'node:fs';
+import { opendir } from 'node:fs/promises';
+import { join } from 'node:path';
+
+/**
+ *
+ * @param path The path in which to find files.
+ * @param filter A function that takes a path and returns true if the path should be included in the results.
+ * Ideally this is a file extension, however you can also provide more parts of the end of the file.
+ *
+ * @return An {@link AsyncIterableIterator<string>} of all the files. To loop over these use `for await (const file of findFilesRecursively(path, filter)) {}`
+ *
+ *
+ * @example
+ * ```typescript
+ * // With CommonJS: To find all files ending with `.ts` in the src directory:
+ * const path = require('node:path');
+ *
+ * for await (const file of findFilesRecursively(path.join(__dirname, 'src'), (filePath) => filePath.endsWith('.ts'))) {
+ *   console.log(file);
+ * }
+ * ```
+ *
+ * @example
+ * ```typescript
+ * // With ESM: To find all files ending with `.ts` in the src directory:
+ * for await (const file of findFilesRecursively(new URL('src', import.meta.url), (filePath) => filePath.endsWith('.ts'))) {
+ *   console.log(file);
+ * }
+ * ```
+ */
+export async function* findFilesRecursively(path: PathLike, filter: (filePath: string) => boolean = () => true): AsyncIterableIterator<string> {
+	try {
+		const dir = await opendir(path);
+
+		for await (const item of dir) {
+			if (item.isFile() && filter(item.name)) {
+				yield join(dir.path, item.name);
+			} else if (item.isDirectory()) {
+				yield* findFilesRecursively(join(dir.path, item.name), filter);
+			}
+		}
+	} catch (error) {
+		if ((error as any).code !== 'ENOENT') {
+			console.error(error);
+		}
+	}
+}
+
+/**
+ *
+ * @param path The path in which to find files. This can be a string, buffer or {@link URL}.
+ * @param fileStartsWith The string pattern with which the file name must start.
+ *
+ * Note that we do **not** support a full globby pattern using asterisk for wildcards. It has to be an exact match with {@link String.startsWith}
+ *
+ * @return An {@link AsyncIterableIterator<string>} of all the files. To loop over these use `for await (const file of findFilesRecursivelyStringStartsWith(path, fileNameEndsWith)) {}`
+ */
+export function findFilesRecursivelyStringStartsWith(path: PathLike, fileStartsWith: string) {
+	return findFilesRecursively(path, (filePath) => filePath.startsWith(fileStartsWith));
+}
+
+/**
+ *
+ * @param path The path in which to find files. This can be a string, buffer or {@link URL}.
+ * @param fileEndsWith The string pattern with which the file name must end.
+ * Ideally this is a file extension, however you can also provide more parts of the end of the file.
+ *
+ * Note that we do **not** support a full globby pattern using asterisk for wildcards. It has to be an exact match with {@link String.endsWith}
+ *
+ * @return An {@link AsyncIterableIterator<string>} of all the files. To loop over these use `for await (const file of findFilesRecursivelyStringEndsWith(path, fileNameEndsWith)) {}`
+ */
+export function findFilesRecursivelyStringEndsWith(path: PathLike, fileEndsWith: string) {
+	return findFilesRecursively(path, (filePath) => filePath.startsWith(fileEndsWith));
+}
+
+/**
+ * @param path The path in which to find files. This can be a string, buffer or {@link URL}.
+ * @param include The string pattern which must be present in the file name.
+ *
+ * Note that we do **not** support a full globby pattern using asterisk for wildcards. It has to be an exact match with {@link String.includes}
+ *
+ * @return An {@link AsyncIterableIterator<string>} of all the files. To loop over these use `for await (const file of findFilesRecursivelyStringIncludes(path, fileNameEndsWith)) {}`
+ */
+export function findFilesRecursivelyStringIncludes(path: PathLike, include: string) {
+	return findFilesRecursively(path, (filePath) => filePath.includes(include));
+}
+
+/**
+ * @param path The path in which to find files. This can be a string, buffer or {@link URL}.
+ * @param regex The regex pattern that the file name must match.
+ *
+ * @return An {@link AsyncIterableIterator<string>} of all the files. To loop over these use `for await (const file of findFilesRecursivelyRegex(path, fileNameEndsWith)) {}`
+ */
+export function findFilesRecursivelyRegex(path: PathLike, regex: RegExp) {
+	return findFilesRecursively(path, (filePath) => regex.test(filePath));
+}
