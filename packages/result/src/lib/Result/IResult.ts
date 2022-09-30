cda4b8a492d6f03e929ae31f6bb21cabb81d5f99
Jeroen Claassens <support@favware.tech>
2022-10-01 01:11:19 +0200

    chore: ugh. bad rebase.
    permalink: http://whatthecommit.com/e1777f69b91bc9a56011eca100c962b6

--- packages/result/src/lib/Result/IResult.ts
+++ packages/result/src/lib/Result/IResult.ts
@@ -449,6 +449,8 @@ export interface IResult<T, E> {
 	 * If the value is an `Err`, it throws a {@link ResultError} with the message, and the content of the `Err`.
 	 * @seealso {@link unwrapOr}
 	 * @seealso {@link unwrapOrElse}
+	 * @seealso {@link unwrapErr}
+	 * @seealso {@link unwrapRaw}
 	 *
 	 * @example
 	 * ```typescript
@@ -473,6 +475,10 @@ export interface IResult<T, E> {
 	 * Returns the contained `Err` value.
 	 *
 	 * If the value is an `Ok`, it throws a {@link ResultError} with the message, and the content of the `Ok`.
+	 * @seealso {@link unwrap}
+	 * @seealso {@link unwrapOr}
+	 * @seealso {@link unwrapOrElse}
+	 * @seealso {@link unwrapRaw}
 	 *
 	 * @example
 	 * ```typescript
@@ -498,6 +504,11 @@ export interface IResult<T, E> {
 	 *
 	 * Arguments passed to `unwrapOr` are eagerly evaluated; if you are passing the result of a function call, it is
 	 * recommended to use {@link unwrapOrElse}, which is lazily evaluated.
+	 * @seealso {@link unwrap}
+	 * @seealso {@link unwrapOrElse}
+	 * @seealso {@link unwrapErr}
+	 * @seealso {@link unwrapRaw}
+	 *
 	 * @param defaultValue The default value.
 	 *
 	 * @example
@@ -517,6 +528,11 @@ export interface IResult<T, E> {
 
 	/**
 	 * Returns the contained `Ok` value or computes it from a closure.
+	 * @seealso {@link unwrap}
+	 * @seealso {@link unwrapOr}
+	 * @seealso {@link unwrapErr}
+	 * @seealso {@link unwrapRaw}
+	 *
 	 * @param op The predicate.
 	 *
 	 * @example
@@ -538,6 +554,7 @@ export interface IResult<T, E> {
 	 * @seealso {@link unwrap}
 	 * @seealso {@link unwrapOr}
 	 * @seealso {@link unwrapOrElse}
+	 * @seealso {@link unwrapErr}
 	 *
 	 * @example
 	 * ```typescript
