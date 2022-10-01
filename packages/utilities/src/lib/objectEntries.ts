4d8eb5c90a721dae2bd7ab2765f564b9ce128339
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-10-01 09:40:54 -0400

    fix(objectEntires): return type
    Co-authored-by: imranbarbhuiya

--- packages/utilities/src/lib/objectEntries.ts
+++ packages/utilities/src/lib/objectEntries.ts
@@ -1,5 +1,5 @@
 import type { NonNullObject } from './utilityTypes';
 
-export function objectEntries<T extends NonNullObject>(obj: T): [keyof T, T[keyof T]][] {
-	return Object.entries(obj) as [keyof T, T[keyof T]][];
+export function objectEntries<T extends NonNullObject>(obj: T): T extends ArrayLike<any> ? [number, T[number]][] : [keyof T, T[keyof T]][] {
+	return Object.entries(obj) as T extends ArrayLike<any> ? [number, T[number]][] : [keyof T, T[keyof T]][];
 }
