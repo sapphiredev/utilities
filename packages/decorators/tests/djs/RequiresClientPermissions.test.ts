b0e813b8c73f57a614bee5c5339552dd5b61d29f
r-priyam <roy_priyam@outlook.com>
2022-10-01 08:35:30 +0530

    fix: initial attempt to fix some test

--- packages/decorators/tests/djs/RequiresClientPermissions.test.ts
+++ packages/decorators/tests/djs/RequiresClientPermissions.test.ts
@@ -1,10 +1,18 @@
 import { UserError } from '@sapphire/framework';
-import { Message as DJSMessage, PermissionResolvable, Permissions, PermissionString } from 'discord.js';
+import {
+	ChannelType,
+	Message as DJSMessage,
+	PermissionFlagsBits,
+	PermissionResolvable,
+	Permissions,
+	PermissionsBitField,
+	PermissionsString
+} from 'discord.js';
 import diff from 'lodash/difference';
 import { DecoratorIdentifiers, RequiresClientPermissions } from '../../src';
 
 interface BitField {
-	missing(resolvedPermissions: Permissions): PermissionString[];
+	missing(resolvedPermissions: Permissions): PermissionsString[];
 }
 
 interface Message {
@@ -18,7 +26,7 @@ interface Message {
 }
 
 function buildMissing(resolvedPermissions: Permissions, ...givenPermissions: PermissionResolvable[]) {
-	return diff(resolvedPermissions.toArray(), new Permissions(givenPermissions).toArray());
+	return diff(resolvedPermissions.toArray(), new PermissionsBitField(givenPermissions).toArray());
 }
 
 function buildMessage(channelType: DJSMessage['channel']['type'], ...givenPermissions: PermissionResolvable[]): Message {
@@ -36,7 +44,7 @@ function buildMessage(channelType: DJSMessage['channel']['type'], ...givenPermis
 describe('RequiresClientPermissions', () => {
 	describe('WITH DM-compatible permissions', () => {
 		class Test {
-			@RequiresClientPermissions(['SEND_MESSAGES', 'ATTACH_FILES'])
+			@RequiresClientPermissions(['SendMessages', 'AttachFiles'])
 			public getValue(_message: Message) {
 				return Promise.resolve('Resolved');
 			}
@@ -44,36 +52,38 @@ describe('RequiresClientPermissions', () => {
 
 		const instance = new Test();
 
-		describe('WITH channel === GUILD_TEXT', () => {
+		describe('WITH channel === GuildTest', () => {
 			test('GIVEN has permission THEN returns resolved', async () => {
-				const result = await instance.getValue(buildMessage('GUILD_TEXT', 'SEND_MESSAGES', 'ATTACH_FILES'));
+				const result = await instance.getValue(
+					buildMessage(ChannelType.GuildText, PermissionFlagsBits.SendMessages, PermissionFlagsBits.AttachFiles)
+				);
 
 				expect(result).toBe('Resolved');
 			});
 
 			test('GIVEN lacking 1 permission THEN throws UserError', async () => {
-				const result = instance.getValue(buildMessage('GUILD_TEXT', 'SEND_MESSAGES'));
+				const result = instance.getValue(buildMessage(ChannelType.GuildText, PermissionFlagsBits.SendMessages));
 
 				await expect(result).rejects.toThrowError(
 					new UserError({
 						identifier: DecoratorIdentifiers.RequiresClientPermissionsMissingPermissions,
-						message: `Sorry, but I am not allowed to do that. I am missing the permissions: ATTACH_FILES`,
+						message: `Sorry, but I am not allowed to do that. I am missing the permissions: AttachFiles`,
 						context: {
-							missing: ['ATTACH_FILES']
+							missing: ['AttachFiles']
 						}
 					})
 				);
 			});
 
 			test('GIVEN lacking 2 permissions THEN throws UserError', async () => {
-				const result = instance.getValue(buildMessage('GUILD_TEXT'));
+				const result = instance.getValue(buildMessage(ChannelType.GuildText));
 
 				await expect(result).rejects.toThrowError(
 					new UserError({
 						identifier: DecoratorIdentifiers.RequiresClientPermissionsMissingPermissions,
-						message: `Sorry, but I am not allowed to do that. I am missing the permissions: SEND_MESSAGES,ATTACH_FILES`,
+						message: `Sorry, but I am not allowed to do that. I am missing the permissions: SendMessages,AttachFiles`,
 						context: {
-							missing: ['SEND_MESSAGES', 'ATTACH_FILES']
+							missing: ['SendMessages', 'AttachFiles']
 						}
 					})
 				);
@@ -82,7 +92,7 @@ describe('RequiresClientPermissions', () => {
 
 		describe('WITH channel === DM', () => {
 			test('GIVEN no additional permissions THEN resolves', async () => {
-				const result = await instance.getValue(buildMessage('DM'));
+				const result = await instance.getValue(buildMessage(ChannelType.DM));
 
 				expect(result).toBe('Resolved');
 			});
@@ -91,7 +101,7 @@ describe('RequiresClientPermissions', () => {
 
 	describe('WITH DM-incompatible permissions', () => {
 		class Test {
-			@RequiresClientPermissions(['MANAGE_MESSAGES', 'ADD_REACTIONS', 'EMBED_LINKS'])
+			@RequiresClientPermissions([PermissionFlagsBits.ManageMessages, PermissionFlagsBits.AddReactions, PermissionFlagsBits.EmbedLinks])
 			public getValue(_message: Message) {
 				return Promise.resolve('Resolved');
 			}
@@ -101,7 +111,7 @@ describe('RequiresClientPermissions', () => {
 
 		describe('WITH channel === DM', () => {
 			test('GIVEN no additional permissions THEN resolves', async () => {
-				const result = instance.getValue(buildMessage('DM'));
+				const result = instance.getValue(buildMessage(ChannelType.DM));
 
 				await expect(result).rejects.toThrowError(
 					new UserError({
