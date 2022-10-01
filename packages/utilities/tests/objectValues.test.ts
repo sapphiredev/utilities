577ddfc129689d1d1a6ff67d239cea6e709f57aa
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-10-01 09:14:32 -0400

    test(objectValues): cover readonly types

--- packages/utilities/tests/objectValues.test.ts
+++ packages/utilities/tests/objectValues.test.ts
@@ -1,7 +1,21 @@
 import { objectValues } from '../src';
 
 describe('objectValues', () => {
-	test('GIVEN basic THEN returns expected', () => {
+	test('GIVEN basic readonly THEN returns expected', () => {
+		const source = { a: 'Hello', b: 420 } as const;
+		const expected = ['Hello', 420];
+
+		expect<('Hello' | 420)[]>(objectValues(source)).toEqual(expected);
+	});
+
+	test('GIVEN deep readonly THEN returns expected', () => {
+		const source = { a: 'Hello', b: 420, deep: { i: [] } } as const;
+		const expected = ['Hello', 420, { i: [] }];
+
+		expect<('Hello' | 420 | { i: readonly [] })[]>(objectValues(source)).toEqual(expected);
+	});
+
+	test('GIVEN basic readonly THEN returns expected', () => {
 		const source = { a: 'Hello', b: 420 };
 		const expected = ['Hello', 420];
 
