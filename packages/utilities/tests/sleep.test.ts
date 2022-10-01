8f2d5ac88e5434acaeb0c6d441d08b9325b65cc4
parbez <imranbarbhuiya.fsd@gmail.com>
2022-10-01 20:05:16 +0530

    fix: add 51 too

--- packages/utilities/tests/sleep.test.ts
+++ packages/utilities/tests/sleep.test.ts
@@ -56,13 +56,13 @@ describe('sleepSync', () => {
 	test('GIVEN a number of ms THEN return after that time', () => {
 		const start = Date.now();
 		sleepSync(50);
-		expect(Date.now() - start).toBe(50);
+		expect(Date.now() - start).oneOf([50, 51]);
 	});
 
 	test('GIVEN a number of ms and a value THEN return after that time with the value', () => {
 		const start = Date.now();
 		const value = sleepSync(50, 'test');
-		expect(Date.now() - start).toBe(50);
+		expect(Date.now() - start).oneOf([50, 51]);
 		expect<string>(value).toBe('test');
 	});
 });
