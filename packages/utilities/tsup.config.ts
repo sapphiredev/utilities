c0035c015b3eccd1321c1373aa20797e3d0c659e
Jeroen Claassens <support@favware.tech>
2022-10-02 16:25:22 +0200

    fix(utilities): fixed output file extensions

--- packages/utilities/tsup.config.ts
+++ packages/utilities/tsup.config.ts
@@ -2,8 +2,24 @@ import { createTsupConfig } from '../../scripts/tsup.config';
 
 export default createTsupConfig({
 	target: 'es2019',
-	bundle: false,
+	bundle: true,
 	entry: ['src/**/*.ts', '!src/**/*.d.ts'],
 	format: ['esm', 'cjs'],
-	dts: false
+	dts: false,
+	esbuildPlugins: [
+		{
+			name: 'add-mjs-to-file-imports-and-exports',
+			setup(build) {
+				const isEsm = build.initialOptions.define?.TSUP_FORMAT === '"esm"';
+				build.onResolve({ filter: /.*/ }, (args) => {
+					if (args.importer) {
+						return {
+							path: `${args.path}.${isEsm ? 'mjs' : 'js'}`,
+							external: true
+						};
+					}
+				});
+			}
+		}
+	]
 });
