84d3810c4310dd80741e72ea95bc079ab49ee210
Jeroen Claassens <support@favware.tech>
2022-10-08 18:46:58 +0200

    chore: hmmm
    permalink: http://whatthecommit.com/d80df00961644ea75c3ce283ac3d7f32

--- packages/utilities/src/lib/pickRandom.ts
+++ packages/utilities/src/lib/pickRandom.ts
@@ -1,12 +1,20 @@
 /**
  * Picks a random element from an array
  * @param array The array to pick a random element from
+ * @param amount - Amount of values to obtain randomly
  */
-export function pickRandom<T>(array: readonly T[], count?: 1): T;
-export function pickRandom<T>(array: readonly T[], count: number): T[];
-export function pickRandom<T>(array: readonly T[], count = 1): T | T[] {
+export function pickRandom<T>(array: readonly T[], amount?: 1): T;
+export function pickRandom<T>(array: readonly T[], amount: number): T[];
+export function pickRandom<T>(array: readonly T[], amount = 1): T | T[] {
 	const arr = [...array];
-	if (typeof count === 'undefined') return arr[Math.floor(Math.random() * arr.length)];
-	if (!arr.length || !count) return [];
-	return Array.from({ length: Math.min(count, arr.length) }, (): T => arr.splice(Math.floor(Math.random() * arr.length), 1)[0]!);
+
+	if (typeof amount === 'undefined') {
+		return arr[Math.floor(Math.random() * arr.length)];
+	}
+
+	if (!arr.length || !amount) {
+		return [];
+	}
+
+	return Array.from({ length: Math.min(amount, arr.length) }, (): T => arr.splice(Math.floor(Math.random() * arr.length), 1)[0]!);
 }
