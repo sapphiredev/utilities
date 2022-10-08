6d220055a0e3791a77a69870f0c919a4d12b07d7
Goestav <27970303+goestav@users.noreply.github.com>
2022-10-09 00:23:32 +0200

    fix(utilities): race condition issue for closed paginated message (#484)
    * fix(utilities): race condition issue for closed paginated message
    
    This commit fixes a bug where the button components won't get removed
    due to a race condition error:
    `DiscordAPIError: Interaction has already been acknowledged.`.
    This occurs when trying to interact with the message when it is about to
    close the paginated message.
    
    Co-authored-by: Jeroen Claassens <support@favware.tech>

--- packages/discord.js-utilities/src/lib/PaginatedMessages/PaginatedMessage.ts
+++ packages/discord.js-utilities/src/lib/PaginatedMessages/PaginatedMessage.ts
@@ -1142,7 +1142,12 @@ export class PaginatedMessage {
 	 * Handles the `end` event from the collector.
 	 * @param reason The reason for which the collector was ended.
 	 */
-	protected handleEnd(_: Collection<Snowflake, ButtonInteraction | SelectMenuInteraction>, reason: string): void {
+	protected async handleEnd(_: Collection<Snowflake, ButtonInteraction | SelectMenuInteraction>, reason: string): Promise<void> {
+		// Ensure no race condition can occur where interacting with the message when the paginated message closes would otherwise result in a DiscordAPIError
+		if (this.response !== null && isAnyInteraction(this.response) && this.response.isMessageComponent()) {
+			this.response.message = await this.response.fetchReply();
+		}
+
 		// Remove all listeners from the collector:
 		this.collector?.removeAllListeners();
 
