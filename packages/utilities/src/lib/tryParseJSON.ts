3e4901bdaa7c6918b40346e9b50a47778f956a0a
Voxelli <69213593+legendhimslef@users.noreply.github.com>
2022-10-08 20:39:57 +0530

    feat: added `tryParseURL` (#482)
    Co-authored-by: Jeroen Claassens <support@favware.tech>

--- packages/utilities/src/lib/tryParseJSON.ts
+++ packages/utilities/src/lib/tryParseJSON.ts
@@ -2,10 +2,12 @@
  * Try parse a stringified JSON string.
  * @param value The value to parse
  */
-export function tryParse(value: string): object | string {
+export function tryParseJSON(value: string): object | string | number {
 	try {
 		return JSON.parse(value);
 	} catch (err) {
 		return value;
 	}
 }
+
+export { tryParseJSON as tryParse };
