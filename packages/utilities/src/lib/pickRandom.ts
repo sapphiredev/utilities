3dc603ca786fc9b67cc2cc4d36cbabdab6705994
Jeroen Claassens <support@favware.tech>
2022-10-08 19:03:58 +0200

    chore: git stash license.md node_modules package.json packages readme.md scripts tsconfig.base.json tsconfig.dev.json tsconfig.eslint.json turbo.json typedoc.json vitest.config.ts yarn.lock
    permalink: http://whatthecommit.com/7ad7c71289d30791e2f20c54465142aa

--- packages/utilities/src/lib/pickRandom.ts
+++ packages/utilities/src/lib/pickRandom.ts
@@ -1,7 +1,7 @@
 /**
  * Picks a random element from an array
  * @param array The array to pick a random element from
- * @param amount - Amount of values to obtain randomly
+ * @param amount Amount of values to obtain randomly
  */
 export function pickRandom<T>(array: readonly T[], amount?: 1): T;
 export function pickRandom<T>(array: readonly T[], amount: number): T[];
@@ -16,5 +16,5 @@ export function pickRandom<T>(array: readonly T[], amount = 1): T | T[] {
 		return [];
 	}
 
-	return Array.from({ length: Math.min(amount, arr.length) }, (): T => arr.splice(Math.floor(Math.random() * arr.length), 1)[0]!);
+	return Array.from({ length: Math.min(amount, arr.length) }, () => arr.splice(Math.floor(Math.random() * arr.length), 1)[0]);
 }
