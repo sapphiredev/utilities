d0e8adcad2cff8889569f18ba8e8979bd1d2bd5d
Jeroen Claassens <support@favware.tech>
2022-10-08 19:04:20 +0200

    chore: this really should not take 19 minutes to build.
    permalink: http://whatthecommit.com/ff1dccf3e15376dc20ceafc1a977aebe

--- packages/utilities/src/lib/pickRandom.ts
+++ packages/utilities/src/lib/pickRandom.ts
@@ -1,7 +1,7 @@
 /**
  * Picks a random element from an array
  * @param array The array to pick a random element from
- * @param amount Amount of values to obtain randomly
+ * @param amount Amount of values to obtain randomly (default: 1)
  */
 export function pickRandom<T>(array: readonly T[], amount?: 1): T;
 export function pickRandom<T>(array: readonly T[], amount: number): T[];
