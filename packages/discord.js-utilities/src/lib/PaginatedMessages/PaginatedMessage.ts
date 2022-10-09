sapphiredev/utilities

commit 8c9c9b750408ac2fb4c034385dae1e55b6ce6ccf

fix(utilities): race condition issue for closed paginated message (#484)
* fix(utilities): race condition issue for closed paginated message

This commit fixes a bug where the button components won't get removed
due to a race condition error:
`DiscordAPIError: Interaction has already been acknowledged.`.
This occurs when trying to interact with the message when it is about to
close the paginated message.

Co-authored-by: Jeroen Claassens <support@favware.tech>

packages/discord.js-utilities/src/lib/PaginatedMessages/PaginatedMessage.ts
@@ -1142,7 +1142,12 @@ export class PaginatedMessage {
 	 * Handles the `end` event from the collector.
 	 * @param reason The reason for which the collector was ended.
 	 */
-	protected handleEnd(_: Collection<Snowflake, ButtonInteraction | SelectMenuInteraction>, reason: string): void {
+	protected async handleEnd(_: Collection<Snowflake, ButtonInteraction | SelectMenuInteraction>, reason: string): Promise<void> {
+		// Ensure no race condition can occur where interacting with the message when the paginated message closes would otherwise result in a DiscordAPIError
+		if (this.response !== null && isAnyInteraction(this.response) && this.response.isMessageComponent()) {
+			this.response.message = await this.response.fetchReply();
+		}
+
 		// Remove all listeners from the collector:
 		this.collector?.removeAllListeners();
 

commit d5a9fc6329e03eb601e66bfc775c9754d9719a3c

fix(discord.js-utilities): fixed stop button crashing bots
Regression issue caused by #484

packages/discord.js-utilities/src/lib/PaginatedMessages/PaginatedMessage.ts
@@ -31,6 +31,7 @@ import type {
 	PaginatedMessageOptions,
 	PaginatedMessagePage,
 	PaginatedMessageSelectMenuOptionsFunction,
+	PaginatedMessageStopReasons,
 	PaginatedMessageWrongUserInteractionReplyFunction
 } from './PaginatedMessageTypes';
 import { actionIsButtonOrMenu, createPartitionedMessageRow, isMessageButtonInteraction, safelyReplyToInteraction } from './utils';
@@ -1142,9 +1143,17 @@ export class PaginatedMessage {
 	 * Handles the `end` event from the collector.
 	 * @param reason The reason for which the collector was ended.
 	 */
-	protected async handleEnd(_: Collection<Snowflake, ButtonInteraction | SelectMenuInteraction>, reason: string): Promise<void> {
+	protected async handleEnd(
+		_: Collection<Snowflake, ButtonInteraction | SelectMenuInteraction>,
+		reason: PaginatedMessageStopReasons
+	): Promise<void> {
 		// Ensure no race condition can occur where interacting with the message when the paginated message closes would otherwise result in a DiscordAPIError
-		if (this.response !== null && isAnyInteraction(this.response) && this.response.isMessageComponent()) {
+		if (
+			(reason === 'time' || reason === 'idle') &&
+			this.response !== null &&
+			isAnyInteraction(this.response) &&
+			this.response.isMessageComponent()
+		) {
 			this.response.message = await this.response.fetchReply();
 		}
 

packages/discord.js-utilities/src/lib/PaginatedMessages/PaginatedMessageTypes.ts
@@ -198,3 +198,15 @@ export interface SafeReplyToInteractionParameters<T extends 'edit' | 'reply' | n
 	messageMethod?: T;
 	messageMethodContent?: T extends 'reply' ? ReplyMessageOptions : MessageEditOptions;
 }
+
+export type PaginatedMessageStopReasons =
+	| 'time'
+	| 'idle'
+	| 'user'
+	| 'messageDelete'
+	| 'channelDelete'
+	| 'threadDelete'
+	| 'guildDelete'
+	| 'limit'
+	| 'componentLimit'
+	| 'userLimit';