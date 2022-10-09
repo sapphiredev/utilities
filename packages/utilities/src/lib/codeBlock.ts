187b193a7c910d818b094056588de599b7a72336
Jeroen Claassens <support@favware.tech>
2022-10-09 19:19:43 +0200

    refactor(utilities): improve `codeBlock` and `inlineCodeBlock`

--- packages/utilities/src/lib/codeBlock.ts
+++ packages/utilities/src/lib/codeBlock.ts
@@ -1,14 +1,32 @@
 const zws = String.fromCharCode(8203);
 
 /**
- * Wraps text in a markdown codeblock with optionally a language indicator for syntax highlighting
- * @param language The codeblock language
- * @param expression The expression to be wrapped in the codeblock
+ * Wraps the content inside a codeblock with no language
+ *
+ * @remark If the provided content includes 3 backticks (```) then those backticks will be escaped
+ * by adding a [Zero Width Space](https://en.wikipedia.org/wiki/Zero-width_space) between the first and second backtick
+ *
+ * @remark If the provided content ends with a backtick then a [Zero Width Space](https://en.wikipedia.org/wiki/Zero-width_space) will be added
+ * to the end of the content
+ *
+ * @param content - The content to wrap
  */
-export function codeBlock<T>(language: string, expression: T): string {
-	if (typeof expression === 'string') {
-		if (expression.length === 0) return `\`\`\`${zws}\`\`\``;
-		return `\`\`\`${language}\n${expression.replace(/```/, `\`${zws}\`\``).replace(/`$/g, `\`${zws}`)}\`\`\``;
-	}
-	return `\`\`\`${language}\n${expression || zws}\`\`\``;
+export function codeBlock<C extends string>(content: C): `\`\`\`\n${C}\n\`\`\``;
+
+/**
+ * Wraps the content inside a codeblock with the specified language
+ *
+ * @remark If the provided content includes 3 backticks (```) then those backticks will be escaped
+ * by adding a [Zero Width Space](https://en.wikipedia.org/wiki/Zero-width_space) between the first and second backtick
+ *
+ * @remark If the provided content ends with a backtick then a [Zero Width Space](https://en.wikipedia.org/wiki/Zero-width_space) will be added
+ * to the end of the content
+ *
+ * @param language The codeblock language
+ * @param content The expression to be wrapped in the codeblock
+ */
+export function codeBlock<L extends string, C extends string>(language: L, content: C): `\`\`\`${L}\n${C}\n\`\`\``;
+export function codeBlock(...args: [string, string?]): string {
+	const [language, content] = args.length === 1 ? ['', args[0]] : args;
+	return `\`\`\`${language}\n${String(content).replace(/```/, `\`${zws}\`\``).replace(/`$/g, `\`${zws}`)}\n\`\`\``;
 }
--- packages/utilities/src/lib/inlineCodeBlock.ts
+++ packages/utilities/src/lib/inlineCodeBlock.ts
@@ -2,8 +2,15 @@ const zws = String.fromCharCode(8203);
 
 /**
  * Wraps text in a markdown inline codeblock
- * @param expression The expression to be wrapped in the codeblock
+ * @param content The expression to be wrapped in the codeblock
  */
-export function inlineCodeBlock(input: string): string {
-	return `\`${input.replace(/ /g, '\u00A0').replace(/`/g, `\`${zws}`)}\``;
+export function inlineCodeBlock<C extends string>(content: C): `\`${C}\`` {
+	// Replace spaces with non-breaking spaces
+	content = content.replace(/ /g, '\u00A0') as C;
+
+	// Replace backticks with zero-width-space escaped backticks
+	content = content.replace(/`/g, `\`${zws}`) as C;
+
+	// Return the input wrapped in backticks
+	return `\`${content}\``;
 }
