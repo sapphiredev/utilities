fdd72a153042cd0e49c9efb771569d075b69cc5e
Jeroen Claassens <support@favware.tech>
2022-10-22 10:51:29 +0200

    refactor(utilities): improve `codeBlock` and `inlineCodeBlock` (#498)
    * refactor(utilities): improve `codeBlock` and `inlineCodeBlock`
    
    * chore: shit code!
    
    permalink: http://whatthecommit.com/ca4e9c4c53a5fd8e8217f41e76924d83

--- packages/utilities/tests/codeBlock.test.ts
+++ packages/utilities/tests/codeBlock.test.ts
@@ -4,12 +4,15 @@ const zeroWidthSpace = String.fromCharCode(8203);
 
 describe('codeBlock', () => {
 	test('GIVEN expression w/o length THEN returns wrapped ZeroWidthSpace', () => {
-		expect(codeBlock('md', '')).toStrictEqual(`\`\`\`${zeroWidthSpace}\`\`\``);
+		expect(codeBlock('md', '')).toStrictEqual(`\`\`\`md
+
+\`\`\``);
 	});
 
 	test('GIVEN expression w/ length THEN returns expressed wrapped in markdown', () => {
 		expect(codeBlock('md', '# Header')).toStrictEqual(`\`\`\`md
-# Header\`\`\``);
+# Header
+\`\`\``);
 	});
 
 	test('GIVEN expression w/ length w/ inner code block THEN returns expressed wrapped in markdown', () => {
@@ -23,13 +26,16 @@ describe('codeBlock', () => {
 			\`\`\`
 		`
 			)
-		).toStrictEqual(`\`\`\`md
+		).toStrictEqual(
+			`\`\`\`md
 
 			# Header
-			\`​\`\`js
+			\`${zeroWidthSpace}\`\`js
 				if (sapphireCommunityIsCool) return 'awesome!';
 			\`\`\`
-		\`\`\``);
+		
+\`\`\``
+		);
 	});
 
 	test('GIVEN expression w/ length w/ inner inline code block THEN returns expressed wrapped in markdown', () => {
@@ -45,16 +51,21 @@ describe('codeBlock', () => {
 
 			# Header
 			\`const sapphireCommunityIsCool = true\`
-		\`\`\``);
+		
+\`\`\``);
 	});
 
 	test('GIVEN expression of non-string type THEN returns wrapped expression', () => {
+		// @ts-expect-error Checking for invalid input
 		expect(codeBlock('md', 123456789)).toStrictEqual(`\`\`\`md
-123456789\`\`\``);
+123456789
+\`\`\``);
 	});
 
 	test('GIVEN expression of falsey type THEN returns wrapped expression', () => {
+		// @ts-expect-error Checking for invalid input
 		expect(codeBlock('md', false)).toStrictEqual(`\`\`\`md
-${zeroWidthSpace}\`\`\``);
+false
+\`\`\``);
 	});
 });
