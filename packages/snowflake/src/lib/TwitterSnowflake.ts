61d9c2c21539923fa13edc84c8e76d238f9cb557
amber <A06000208@outlook.com>
2022-11-18 08:55:48 -0600

    Update twitter epoch and comment

--- packages/snowflake/src/lib/TwitterSnowflake.ts
+++ packages/snowflake/src/lib/TwitterSnowflake.ts
@@ -3,6 +3,6 @@ import { Snowflake } from './Snowflake';
 /**
  * A class for parsing snowflake ids using Twitter's snowflake epoch
  *
- * Which is 2006-03-21 at 20:50:14.000 UTC+0, the time and date of the first tweet ever made {@linkplain https://twitter.com/jack/status/20}
+ * Which is 2010-11-04 at 20:42:54.657 UTC+0, found in the archived snowflake repository {@linkplain https://github.com/twitter-archive/snowflake/blob/b3f6a3c6ca8e1b6847baa6ff42bf72201e2c2231/src/main/scala/com/twitter/service/snowflake/IdWorker.scala#L25}
  */
-export const TwitterSnowflake = new Snowflake(1142974214000n);
+export const TwitterSnowflake = new Snowflake(1288834974657n);
