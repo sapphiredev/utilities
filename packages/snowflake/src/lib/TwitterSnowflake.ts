f7be4f962f5e7487b04151e5b89038a59a9b7144
amber <A06000208@outlook.com>
2022-11-18 08:59:52 -0600

    Correct given time to be in UTC+0

--- packages/snowflake/src/lib/TwitterSnowflake.ts
+++ packages/snowflake/src/lib/TwitterSnowflake.ts
@@ -3,6 +3,6 @@ import { Snowflake } from './Snowflake';
 /**
  * A class for parsing snowflake ids using Twitter's snowflake epoch
  *
- * Which is 2010-11-04 at 20:42:54.657 UTC+0, found in the archived snowflake repository {@linkplain https://github.com/twitter-archive/snowflake/blob/b3f6a3c6ca8e1b6847baa6ff42bf72201e2c2231/src/main/scala/com/twitter/service/snowflake/IdWorker.scala#L25}
+ * Which is 2010-11-04 at 01:42:54.657 UTC+0, found in the archived snowflake repository {@linkplain https://github.com/twitter-archive/snowflake/blob/b3f6a3c6ca8e1b6847baa6ff42bf72201e2c2231/src/main/scala/com/twitter/service/snowflake/IdWorker.scala#L25}
  */
 export const TwitterSnowflake = new Snowflake(1288834974657n);
