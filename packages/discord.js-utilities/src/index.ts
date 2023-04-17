7d0aad012a223728f4f9129575cdc9abb91e14b4
Jeroen Claassens <support@favware.tech>
2023-04-17 19:32:42 +0200

    fix(discord.js-utilities): add tslib dependency to fix __exportStar not being defined

--- packages/discord.js-utilities/src/index.ts
+++ packages/discord.js-utilities/src/index.ts
@@ -1,7 +1,9 @@
+import 'tslib';
+
 export * from '@sapphire/discord-utilities';
-export * from './lib/builders/MessageBuilder';
 export * from './lib/MessagePrompter';
 export * from './lib/PaginatedMessages';
+export * from './lib/builders/MessageBuilder';
 export * from './lib/type-guards';
 export * from './lib/utilities';
 export * from './lib/utility-types';
