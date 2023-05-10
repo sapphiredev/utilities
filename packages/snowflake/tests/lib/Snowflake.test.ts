7acabc0caa2a5c916b0aef9ab7015a296ff5d657
Aura Román <kyradiscord@gmail.com>
2023-05-10 15:35:51 +0200

    feat(snowflake): expose `processId` and `workerId` (#595)

--- packages/snowflake/tests/lib/Snowflake.test.ts
+++ packages/snowflake/tests/lib/Snowflake.test.ts
@@ -13,7 +13,52 @@ describe('Snowflake', () => {
 		vi.useRealTimers();
 	});
 
-	describe('Generate', () => {
+	describe('processId', () => {
+		test('GIVEN default THEN returns 1n', () => {
+			const snowflake = new Snowflake(sampleEpoch);
+			expect(snowflake.processId).toBe(1n);
+		});
+
+		test.each([15, 15n])('GIVEN valid value (%s) THEN returns same value as bigint', (value) => {
+			const snowflake = new Snowflake(sampleEpoch);
+			snowflake.processId = value;
+			expect(snowflake.processId).toBe(15n);
+		});
+
+		test.each([4200, 4200n])('GIVEN out-of-range value (%s) THEN returns masked value as bigint', (value) => {
+			const snowflake = new Snowflake(sampleEpoch);
+			snowflake.processId = value;
+			expect(snowflake.processId).toBe(8n);
+		});
+	});
+
+	describe('workerId', () => {
+		test('GIVEN default THEN returns 0n', () => {
+			const snowflake = new Snowflake(sampleEpoch);
+			expect(snowflake.workerId).toBe(0n);
+		});
+
+		test.each([15, 15n])('GIVEN valid value (%s) THEN returns same value as bigint', (value) => {
+			const snowflake = new Snowflake(sampleEpoch);
+			snowflake.workerId = value;
+			expect(snowflake.workerId).toBe(15n);
+		});
+
+		test.each([4200, 4200n])('GIVEN out-of-range value (%s) THEN returns masked value as bigint', (value) => {
+			const snowflake = new Snowflake(sampleEpoch);
+			snowflake.workerId = value;
+			expect(snowflake.workerId).toBe(8n);
+		});
+	});
+
+	describe('epoch', () => {
+		test.each([sampleEpoch, Number(sampleEpoch), new Date(Number(sampleEpoch))])('GIVEN %s THEN returns 1577836800000n', (value) => {
+			const snowflake = new Snowflake(value);
+			expect(snowflake.epoch).toBe(sampleEpoch);
+		});
+	});
+
+	describe('generate', () => {
 		test('GIVEN timestamp as number THEN returns predefined snowflake', () => {
 			const testId = '3971046231244804096';
 			const testTimestamp = 2524608000000;
@@ -44,7 +89,7 @@ describe('Snowflake', () => {
 		test('GIVEN empty object options THEN returns predefined snowflake', () => {
 			const testId = '4096';
 			const snowflake = new Snowflake(sampleEpoch);
-			const snow = snowflake.generate({});
+			const snow = snowflake.generate();
 
 			expect(snow.toString()).toBe(testId);
 		});
@@ -52,7 +97,7 @@ describe('Snowflake', () => {
 		test('GIVEN no options THEN returns predefined snowflake', () => {
 			const testId = '4096';
 			const snowflake = new Snowflake(sampleEpoch);
-			const snow = snowflake.generate({});
+			const snow = snowflake.generate();
 
 			expect(snow.toString()).toBe(testId);
 		});
@@ -109,6 +154,15 @@ describe('Snowflake', () => {
 			expect(snow.toString()).toBe(testId);
 		});
 
+		test('GIVEN overflowing default processId THEN generates ID with truncated processId', () => {
+			const testId = '106496';
+			const snowflake = new Snowflake(sampleEpoch);
+			snowflake.processId = 0b1111_1010n;
+			const snow = snowflake.generate();
+
+			expect(snow.toString()).toBe(testId);
+		});
+
 		test('GIVEN overflowing workerId THEN generates ID with truncated workerId', () => {
 			const testId = '3411968';
 			const snowflake = new Snowflake(sampleEpoch);
@@ -116,9 +170,18 @@ describe('Snowflake', () => {
 
 			expect(snow.toString()).toBe(testId);
 		});
+
+		test('GIVEN overflowing default workerId THEN generates ID with truncated workerId', () => {
+			const testId = '3411968';
+			const snowflake = new Snowflake(sampleEpoch);
+			snowflake.workerId = 0b1111_1010n;
+			const snow = snowflake.generate();
+
+			expect(snow.toString()).toBe(testId);
+		});
 	});
 
-	describe('Deconstruct', () => {
+	describe('deconstruct', () => {
 		test('GIVEN id as string THEN returns data about snowflake', () => {
 			const snowflake = new Snowflake(sampleEpoch);
 
@@ -150,7 +213,7 @@ describe('Snowflake', () => {
 		});
 	});
 
-	describe('Decode', () => {
+	describe('decode', () => {
 		test('GIVEN id as string THEN returns data about snowflake', () => {
 			const snowflake = new Snowflake(sampleEpoch);
 
@@ -182,12 +245,26 @@ describe('Snowflake', () => {
 		});
 	});
 
+	describe('timestampFrom', () => {
+		const snowflake = new Snowflake(sampleEpoch);
+
+		test('GIVEN id as string THEN returns data about snowflake', () => {
+			const timestamp = snowflake.timestampFrom('3971046231244935169');
+			expect(timestamp).toBe(2524608000000);
+		});
+
+		test('GIVEN id as bigint THEN returns data about snowflake', () => {
+			const timestamp = snowflake.timestampFrom(3971046231244935168n);
+			expect(timestamp).toBe(2524608000000);
+		});
+	});
+
 	describe.each([
 		[String, String],
 		[String, BigInt],
 		[BigInt, String],
 		[BigInt, BigInt]
-	])('Compare', (ctorA, ctorB) => {
+	])('compare', (ctorA, ctorB) => {
 		test.each([
 			[ctorA(737141877803057244n), ctorB(254360814063058944n), 1],
 			[ctorA(1737141877803057244n), ctorB(254360814063058944n), 1],
