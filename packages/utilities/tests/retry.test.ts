881ea18e2f5049b82dfa7713bdbd91d003cf28ca
Jeroen Claassens <support@favware.tech>
2023-06-08 00:01:10 +0200

    test: use function mocks

--- packages/utilities/tests/retry.test.ts
+++ packages/utilities/tests/retry.test.ts
@@ -2,9 +2,11 @@ import { retry } from '../src/lib/retry';
 
 describe('retry', () => {
 	test('GIVEN a simple string return THEN returns the same on first attempt', async () => {
-		const result = await retry(() => 'test', 3);
+		const mockFunction = vi.fn().mockReturnValue('test');
+		const result = await retry(mockFunction, 3);
 
 		expect(result).toBe('test');
+		expect(mockFunction).toBeCalledTimes(1);
 	});
 
 	test('GIVEN a twice failing function THEN returns the third result', async () => {
@@ -17,11 +19,13 @@ describe('retry', () => {
 
 			return 'success!';
 		};
+		const mockFunction = vi.fn().mockImplementation(cb);
 
-		const result = await retry(cb, 3);
+		const result = await retry(mockFunction, 3);
 
 		expect(result).toBe('success!');
 		expect(counter).toBe(2);
+		expect(mockFunction).toBeCalledTimes(3);
 	});
 
 	test('GIVEN a thrice failing function WHEN retries is lower THEN returns throws the last error', async () => {
@@ -34,12 +38,16 @@ describe('retry', () => {
 
 			return 'success!';
 		};
+		const mockFunction = vi.fn().mockImplementation(cb);
 
-		await expect(retry(cb, 2)).rejects.toThrowError(Error('💣💥'));
+		await expect(retry(mockFunction, 2)).rejects.toThrowError(Error('💣💥'));
 		expect(counter).toBe(2);
+		expect(mockFunction).toBeCalledTimes(2);
 	});
 
 	test('GIVEN retries below 1 THEN throws', async () => {
-		await expect(retry(() => 'test', 0)).rejects.toThrowError(RangeError('Expected retries to be a number >= 1'));
+		const mockFunction = vi.fn().mockReturnValue('test');
+		await expect(retry(mockFunction, 0)).rejects.toThrowError(RangeError('Expected retries to be a number >= 1'));
+		expect(mockFunction).toBeCalledTimes(0);
 	});
 });
