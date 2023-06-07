be66414f8a114ac85fd55e539d136e8f1e8d906e
Jeroen Claassens <support@favware.tech>
2023-06-07 23:57:01 +0200

    Apply suggestions from code review
    Co-authored-by: Aura Román <kyradiscord@gmail.com>

--- packages/utilities/src/lib/retry.ts
+++ packages/utilities/src/lib/retry.ts
@@ -1,10 +1,9 @@
 import type { Awaitable } from './types';
 
 /**
- * This code is used to retry a function call a specified number of times.
- * The function will retry the call to the callback function until it either succeeds or it runs out of retries.
- * @param  cb The function to be retried is passed in as a callback function.
- * @param  retries The number of retries is also passed in as a parameter.
+ * Calls the callback function until it either succeeds or it runs out of retries.
+ * @param cb The function to be retried is passed in as a callback function.
+ * @param retries The number of retries is also passed in as a parameter.
  * @returns The result of the callback function is returned.
  */
 export async function retry<T>(cb: () => Awaitable<T>, retries: number): Promise<T> {
