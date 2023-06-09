2bfa9a037bfddd1efa56faf123d008697136d560
Jeroen Claassens <support@favware.tech>
2023-06-09 10:26:52 +0200

    feat: add `retrySync`

--- packages/utilities/src/lib/retrySync.ts
+++ packages/utilities/src/lib/retrySync.ts
@@ -1,20 +1,18 @@
-import type { Awaitable } from './types';
-
 /**
- * Asynchronously calls the callback function until it either succeeds or it runs out of retries.
- * For a synchronous variant, see [retrySync](./retrySync.d.ts).
+ * Synchronously calls the callback function until it either succeeds or it runs out of retries.
+ * For an asynchronous variant, see [retry](./retry.d.ts).
  * @param cb The function to be retried is passed in as a callback function.
  * @param retries The number of retries is also passed in as a parameter. Minimum of 0.
  * @returns The result of the callback function is returned.
  */
-export async function retry<T>(cb: () => Awaitable<T>, retries: number): Promise<T> {
+export function retrySync<T>(cb: () => T, retries: number): T {
 	if (retries < 0) throw new RangeError('Expected retries to be a number >= 0');
 	if (retries === 0) return cb();
 
 	let lastError: unknown;
 	for (let i = 0; i < retries; ++i) {
 		try {
-			return await cb();
+			return cb();
 		} catch (error) {
 			lastError = error;
 		}
---
+++ packages/utilities/tests/retrySync.test.ts
@@ -0,0 +1,61 @@
+import { retrySync } from '../src/lib/retrySync';
+
+describe('retry', () => {
+	test('GIVEN a simple string return THEN returns the same on first attempt', () => {
+		const mockFunction = vi.fn().mockReturnValue('test');
+		const result = retrySync(mockFunction, 3);
+
+		expect(result).toBe('test');
+		expect(mockFunction).toBeCalledTimes(1);
+	});
+
+	test('GIVEN a twice failing function THEN returns the third result', () => {
+		let counter = 0;
+		const cb = () => {
+			if (counter < 2) {
+				++counter;
+				throw new Error('💣💥');
+			}
+
+			return 'success!';
+		};
+		const mockFunction = vi.fn().mockImplementation(cb);
+
+		const result = retrySync(mockFunction, 3);
+
+		expect(result).toBe('success!');
+		expect(counter).toBe(2);
+		expect(mockFunction).toBeCalledTimes(3);
+	});
+
+	test('GIVEN a thrice failing function WHEN retries is lower THEN returns throws the last error', () => {
+		let counter = 0;
+		const cb = () => {
+			if (counter < 2) {
+				++counter;
+				throw new Error('💣💥');
+			}
+
+			return 'success!';
+		};
+		const mockFunction = vi.fn().mockImplementation(cb);
+
+		expect(retrySync(mockFunction, 2)).rejects.toThrowError(Error('💣💥'));
+		expect(counter).toBe(2);
+		expect(mockFunction).toBeCalledTimes(2);
+	});
+
+	test('GIVEN 0 retries THEN returns result', () => {
+		const mockFunction = vi.fn().mockReturnValue('test');
+		const result = retrySync(mockFunction, 0);
+
+		expect(result).toBe('test');
+		expect(mockFunction).toBeCalledTimes(1);
+	});
+
+	test('GIVEN retries below 1 THEN throws', () => {
+		const mockFunction = vi.fn().mockReturnValue('test');
+		expect(retrySync(mockFunction, -1)).rejects.toThrowError(RangeError('Expected retries to be a number >= 0'));
+		expect(mockFunction).toBeCalledTimes(0);
+	});
+});
