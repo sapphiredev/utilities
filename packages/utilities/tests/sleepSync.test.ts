cf444df3c4a63ac039de9cf4fe56b86f2068f1e2
Aura Román <kyradiscord@gmail.com>
2023-07-07 21:16:42 +0200

    test(sleepSync): use spies for `Date.now()` (#617)
    We're using a spy for `Date.now()` instead of a fake timer. This is
    because `sleepSync()` does not use timers, but instead checks against
    `Date.now()`'s value.

--- packages/utilities/tests/sleepSync.test.ts
+++ packages/utilities/tests/sleepSync.test.ts
@@ -2,15 +2,23 @@ import { sleepSync } from '../src';
 
 describe('sleepSync', () => {
 	test('GIVEN a number of ms THEN return after that time', () => {
-		const start = Date.now();
-		sleepSync(50);
-		expect(Date.now() - start).oneOf([50, 51]);
+		vi.spyOn(Date, 'now') //
+			.mockReturnValueOnce(0)
+			.mockReturnValueOnce(25)
+			.mockReturnValueOnce(50)
+			.mockReturnValueOnce(50);
+
+		expect<undefined>(sleepSync(50)).toBe(undefined);
+		expect(Date.now()).toBe(50);
 	});
 
 	test('GIVEN a number of ms and a value THEN return after that time with the value', () => {
-		const start = Date.now();
-		const value = sleepSync(50, 'test');
-		expect(Date.now() - start).oneOf([50, 51]);
-		expect<string>(value).toBe('test');
+		vi.spyOn(Date, 'now') //
+			.mockReturnValueOnce(0)
+			.mockReturnValueOnce(50)
+			.mockReturnValueOnce(50);
+
+		expect<string>(sleepSync(50, 'test')).toBe('test');
+		expect(Date.now()).toBe(50);
 	});
 });
