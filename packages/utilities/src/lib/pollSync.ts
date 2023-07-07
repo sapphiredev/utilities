74e93e6f184df0c71131916fcf2049760e5a3bcf
Jeroen Claassens <support@favware.tech>
2023-07-07 21:59:32 +0200

    refactor: review comments

--- packages/utilities/src/lib/pollSync.ts
+++ packages/utilities/src/lib/pollSync.ts
@@ -1,46 +1,41 @@
+import type { PollOptions } from './poll';
 import { sleepSync } from './sleepSync';
 
 /**
  * Executes a function {@link cb} and validates the result with function {@link cbCondition},
- * and repeats this until {@link cbCondition} returns `true` or the {@link timeoutMilliseconds} is reached.
+ * and repeats this until {@link cbCondition} returns `true` or the {@link timeout} is reached.
  *
  * For an asynchronous variant, see [poll](./poll.d.ts).
  * @param cb The function that should be executed.
  * @param cbCondition A function that when given the result of `fn` should return `true` if the polling should stop and should return `false` if the polling should continue.
- * @param intervalMilliseconds The interval in between each poll.
- * @param timeoutMilliseconds The maximum amount of time to poll before timing out and throwing an error.
- * @param verbose Whether to log to the console on each polling interval, allowing the tracing of the amount of required attempts.
- * @returns The result of {@link cb} as soon as {@link cbCondition} returns `true`, or an error if {@link timeoutMilliseconds} is reached.
- * @throws If {@link timeoutMilliseconds} is reached.
+ * @param interval The interval in between each poll.
+ * @param timeout The maximum amount of time to poll before timing out and throwing an error.
+ * @param options Options to provide further modifying behaviour.
+ * @returns The result of {@link cb} as soon as {@link cbCondition} returns `true`, or an error if {@link timeout} is reached.
+ * @throws If {@link timeout} is reached.
  */
-export function pollSync<T>(
-	cb: () => T,
-	cbCondition: (arg: T) => boolean,
-	intervalMilliseconds: number,
-	timeoutMilliseconds: number,
-	verbose?: boolean
-): T {
-	if (timeoutMilliseconds < 0) {
+export function pollSync<T>(cb: () => T, cbCondition: (arg: T) => boolean, interval: number, timeout: number, options: PollOptions = {}): T {
+	if (timeout < 0) {
 		throw new RangeError('Expected timeoutMilliseconds to be a number >= 0');
 	}
 
-	if (timeoutMilliseconds === 0) return cb();
+	if (timeout === 0) return cb();
 
 	const startTime = Date.now();
 	let result = cb();
 
 	while (!cbCondition(result)) {
-		if (verbose) {
-			console.log(`Waiting ${intervalMilliseconds} ms before polling again...`);
+		if (options.verbose) {
+			console.log(`Waiting ${interval} ms before polling again...`);
 		}
 
-		sleepSync(intervalMilliseconds);
+		sleepSync(interval);
 
 		result = cb();
 
 		const currentTime = Date.now();
-		if (currentTime - startTime > timeoutMilliseconds) {
-			throw new Error(`Polling task timed out after ${timeoutMilliseconds} milliseconds`);
+		if (currentTime - startTime > timeout) {
+			throw new Error(`Polling task timed out after ${timeout} milliseconds`);
 		}
 	}
 
