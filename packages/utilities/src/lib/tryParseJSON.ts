7d53836cd97657f0d90c3162710f7f21edacd9a7
Aura Román <kyradiscord@gmail.com>
2023-11-17 22:58:20 +0100

    feat(tryParseJSON): add `boolean` and `null` as possible return types

--- packages/utilities/src/lib/tryParseJSON.ts
+++ packages/utilities/src/lib/tryParseJSON.ts
@@ -3,7 +3,10 @@
  * @param value The string to parse as JSON.
  * @param reviver A function that transforms the results. This function is recursively called for each member of the object.
  */
-export function tryParseJSON(value: string, reviver?: (this: object, key: string, value: unknown) => unknown): object | string | number {
+export function tryParseJSON(
+	value: string,
+	reviver?: (this: object, key: string, value: unknown) => unknown
+): object | string | number | boolean | null {
 	try {
 		return JSON.parse(value, reviver);
 	} catch (err) {
