4374b637e10d7661a44abc034b98af98b327bc9a
Aura Román <kyradiscord@gmail.com>
2023-11-17 22:49:34 +0100

    feat(tryParseJSON): support `replacer` argument

--- packages/utilities/src/lib/tryParseJSON.ts
+++ packages/utilities/src/lib/tryParseJSON.ts
@@ -1,10 +1,11 @@
 /**
  * Try parse a stringified JSON string.
- * @param value The value to parse
+ * @param value The string to parse as JSON.
+ * @param reviver A function that transforms the results. This function is recursively called for each member of the object.
  */
-export function tryParseJSON(value: string): object | string | number {
+export function tryParseJSON(value: string, reviver?: (this: object, key: string, value: unknown) => unknown): object | string | number {
 	try {
-		return JSON.parse(value);
+		return JSON.parse(value, reviver);
 	} catch (err) {
 		return value;
 	}
