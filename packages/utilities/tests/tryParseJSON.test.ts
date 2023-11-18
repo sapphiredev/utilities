4cf0978ed608a48164af0d5a027b72990f32124b
Aura <kyradiscord@gmail.com>
2023-11-18 11:07:19 +0100

    feat(tryParseJSON): support `replacer` argument (#668)
    * feat(tryParseJSON): support `replacer` argument
    
    * feat(tryParseJSON): add `boolean` and `null` as possible return types

--- packages/utilities/tests/tryParseJSON.test.ts
+++ packages/utilities/tests/tryParseJSON.test.ts
@@ -7,6 +7,14 @@ describe('tryParseJSON', () => {
 		expect(tryParseJSON(source)).toEqual(expected);
 	});
 
+	test('GIVEN basic with replacer THEN returns expected', () => {
+		const source = '{"a":4,"b":6}';
+		const expected = { a: '4', b: '6' };
+		const replacer = vi.fn((_key: string, value: unknown) => (typeof value === 'number' ? String(value) : value));
+		expect(tryParseJSON(source, replacer)).toEqual(expected);
+		expect(replacer).toHaveBeenCalledTimes(3);
+	});
+
 	test('GIVEN invalid THEN returns expected', () => {
 		const source = '{"a":4,"b:6}';
 		const expected = '{"a":4,"b:6}';
