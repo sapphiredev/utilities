efb156a9d578760d6c52645604bcc7cd4aa93bdb
Jeroen Claassens <support@favware.tech>
2023-12-04 19:52:17 +0100

    fix(stopwatch): properly split CJS, ESM and IIFE

--- packages/stopwatch/tests/browser/browser.test.ts
+++ packages/stopwatch/tests/browser/browser.test.ts
@@ -19,7 +19,7 @@ describe('browser-bundle-test', () => {
 					 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 					 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 					 <title>BrowserBundleTest</title>
-					 <script>${await readFile(join(__dirname, '../../dist/index.global.js'), 'utf8')}</script>
+					 <script>${await readFile(join(__dirname, '../../dist/iife/index.global.js'), 'utf8')}</script>
 				 </head>
 				 <body></body>
 				 </html>
