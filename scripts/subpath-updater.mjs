5e4984b6bc16a8875ec2272f224ff3ee2e29fc08
Jeroen Claassens <support@favware.tech>
2023-12-04 19:28:26 +0100

    build: prepare splitting CJS, ESM, IIFE properly

--- scripts/subpath-updater.mjs
+++ scripts/subpath-updater.mjs
@@ -2,7 +2,7 @@ import { bold, green, red } from 'colorette';
 import { writeFile } from 'node:fs/promises';
 import { basename } from 'node:path';
 import { format } from 'prettier';
-import { findFilesRecursivelyStringEndsWith } from '../packages/node-utilities/dist/index.mjs';
+import { findFilesRecursivelyStringEndsWith } from '../packages/node-utilities/dist/esm/index.mjs';
 import prettierConfig from '../packages/prettier-config/dist/index.mjs';
 
 const packageName = process.argv[2];
@@ -14,10 +14,15 @@ const exportMap = new Map([
 	[
 		'.',
 		{
-			types: './dist/index.d.ts',
-			import: './dist/index.mjs',
-			require: './dist/index.js',
-			browser: './dist/index.global.js'
+			import: {
+				types: './dist/esm/index.d.mts',
+				default: './dist/esm/index.mjs'
+			},
+			require: {
+				types: './dist/cjs/index.d.ts',
+				default: './dist/cjs/index.cjs'
+			},
+			browser: './dist/iife/index.global.js'
 		}
 	]
 ]);
@@ -26,13 +31,17 @@ for await (const file of findFilesRecursivelyStringEndsWith(new URL(`../packages
 	const name = basename(file).replace(/\.ts$/, '');
 	const splitted = file.split('lib');
 	splitted.shift();
-	const filePath = `./dist/lib${splitted.join('').replace(/\.ts$/, '')}`.replace(/\\/g, '/');
 	if (name === 'index') continue;
 
 	exportMap.set(`./${name}`, {
-		types: `${filePath}.d.ts`,
-		import: `${filePath}.mjs`,
-		require: `${filePath}.js`
+		import: {
+			types: `./dist/esm/lib/${name}.d.mts`,
+			default: `./dist/esm/lib/${name}.mjs`
+		},
+		require: {
+			types: `./dist/cjs/lib/${name}.d.ts`,
+			default: `./dist/cjs/lib/${name}.cjs`
+		}
 	});
 
 	const aliasStoreEntry = aliasStore.get(name);
@@ -40,16 +49,26 @@ for await (const file of findFilesRecursivelyStringEndsWith(new URL(`../packages
 		if (Array.isArray(aliasStoreEntry)) {
 			for (const entry of aliasStoreEntry) {
 				exportMap.set(`./${entry}`, {
-					types: `${filePath}.d.ts`,
-					import: `${filePath}.mjs`,
-					require: `${filePath}.js`
+					import: {
+						types: `./dist/esm/lib/${name}.d.mts`,
+						default: `./dist/esm/lib/${name}.mjs`
+					},
+					require: {
+						types: `./dist/cjs/lib/${name}.d.ts`,
+						default: `./dist/cjs/lib/${name}.cjs`
+					}
 				});
 			}
 		} else {
 			exportMap.set(`./${aliasStoreEntry}`, {
-				types: `${filePath}.d.ts`,
-				import: `${filePath}.mjs`,
-				require: `${filePath}.js`
+				import: {
+					types: `./dist/esm/lib/${name}.d.mts`,
+					default: `./dist/esm/lib/${name}.mjs`
+				},
+				require: {
+					types: `./dist/cjs/lib/${name}.d.ts`,
+					default: `./dist/cjs/lib/${name}.cjs`
+				}
 			});
 		}
 	}
@@ -80,7 +99,7 @@ if (check) {
 	process.exit(1);
 }
 
-const formattedNewPackageJSON = format(newPackageJSON, { ...prettierConfig, parser: 'json-stringify' });
+const formattedNewPackageJSON = await format(newPackageJSON, { ...prettierConfig, parser: 'json-stringify' });
 await writeFile(new URL(`../packages/${packageName}/package.json`, import.meta.url), formattedNewPackageJSON);
 
 console.log(green(`The package.json file for ${packageName} is updated successfully!`));
