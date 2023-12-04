b8afca3594bd54a2856b85558bc768b73998c19f
Jeroen Claassens <support@favware.tech>
2023-12-04 19:28:26 +0100

    build: prepare splitting CJS, ESM, IIFE properly

--- scripts/tsup.config.ts
+++ scripts/tsup.config.ts
@@ -1,18 +1,39 @@
 import { relative, resolve as resolveDir } from 'node:path';
 import { defineConfig, type Options } from 'tsup';
 
-export const createTsupConfig = (options: Options = {}) =>
+const baseOptions: Options = {
+	clean: true,
+	dts: true,
+	entry: ['src/index.ts'],
+	minify: false,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	target: 'es2021',
+	tsconfig: relative(__dirname, resolveDir(process.cwd(), 'src', 'tsconfig.json')),
+	keepNames: true,
+	treeshake: true
+};
+
+export const createTsupConfig = (cjsOptions: Options = {}, esmOptions: Options = cjsOptions, iifeOptions: Options = cjsOptions) => [
 	defineConfig({
-		clean: true,
-		dts: true,
+		...baseOptions,
+		outDir: 'dist/cjs',
+		format: 'cjs',
+		outExtension: () => ({ js: '.cjs' }),
+		...cjsOptions
+	}),
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/esm',
+		format: 'esm',
+		...esmOptions
+	}),
+	defineConfig({
+		...baseOptions,
+		dts: false,
 		entry: ['src/index.ts'],
-		format: ['esm', 'cjs', 'iife'],
-		minify: false,
-		skipNodeModulesBundle: true,
-		sourcemap: true,
-		target: 'es2021',
-		tsconfig: relative(__dirname, resolveDir(process.cwd(), 'src', 'tsconfig.json')),
-		keepNames: true,
-		treeshake: true,
-		...options
-	});
+		outDir: 'dist/iife',
+		format: 'iife',
+		...iifeOptions
+	})
+];
