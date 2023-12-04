e7d72df82c210a4f972a949f213bc734278424dd
Jeroen Claassens <support@favware.tech>
2023-12-04 20:06:03 +0100

    fix(fetch): properly split CJS, ESM and IIFE

--- packages/fetch/tests/fetch.test.ts
+++ packages/fetch/tests/fetch.test.ts
@@ -1,6 +1,6 @@
 import nock from 'nock';
 import { URL as NodeUrl } from 'node:url';
-import { fetch, FetchResultTypes, QueryError, FetchMediaContentTypes } from '../dist';
+import { fetch, FetchResultTypes, QueryError, FetchMediaContentTypes } from '../dist/esm/index.mjs';
 
 describe('fetch', () => {
 	let nockScopeHttp: nock.Scope;
