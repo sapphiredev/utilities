ae1da347440439f3920de15a03cb509306c8798a
Jeroen Claassens <support@favware.tech>
2023-12-10 14:38:52 +0100

    feat(fetch): drop cross-fetch, always use global fetch
    BREAKING CHANGE: This version and all subsequent versions require
    NodeJS v18 or above, or v16 with the `--experimental-fetch` flag

--- packages/fetch/tsup.config.ts
+++ packages/fetch/tsup.config.ts
@@ -1,39 +1,5 @@
-import type { Options } from 'tsup';
 import { createTsupConfig } from '../../scripts/tsup.config';
 
-const options: Options = {
-	esbuildOptions: (options, context) => {
-		switch (context.format) {
-			case 'cjs': {
-				options.banner = {
-					js: [
-						//
-						'"use strict";',
-						'globalThis && globalThis.fetch ? globalThis.fetch : globalThis.fetch = require("cross-fetch");'
-					].join('\n')
-				};
-				break;
-			}
-			case 'esm': {
-				options.banner = {
-					js: [
-						//
-						'import crossFetch from "cross-fetch";',
-						'globalThis && globalThis.fetch ? globalThis.fetch : globalThis.fetch = crossFetch;'
-					].join('\n')
-				};
-				break;
-			}
-			case 'iife': {
-				// iife doesn't need any fetch replacements
-				break;
-			}
-		}
-	}
-};
-
 export default createTsupConfig({
-	cjsOptions: options,
-	esmOptions: options,
-	iifeOptions: { ...options, globalName: 'SapphireFetch' }
+	iifeOptions: { globalName: 'SapphireFetch' }
 });
