80d50660349d3bad5bc69aac2ae735ed6a997e51
Jeroen Claassens <support@favware.tech>
2023-12-10 14:40:48 +0100

    feat(phisherman): drop cross-fetch, always use global fetch
    BREAKING CHANGE: This version and all subsequent versions require
    NodeJS v18 or above, or v16 with the `--experimental-fetch` flag

--- packages/phisherman/tsup.config.ts
+++ packages/phisherman/tsup.config.ts
@@ -1,5 +1,13 @@
+import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
+import { Options } from 'tsup';
 import { createTsupConfig } from '../../scripts/tsup.config';
 
+const defaultOptions: Options = {
+	esbuildPlugins: [esbuildPluginVersionInjector()]
+};
+
 export default createTsupConfig({
+	cjsOptions: defaultOptions,
+	esmOptions: defaultOptions,
 	iifeOptions: { disabled: true }
 });
