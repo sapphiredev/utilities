eccf9f679581d95a7d2b29ae894a6971edcb4cc3
Jeroen Claassens <support@favware.tech>
2023-12-28 18:46:35 +0100

    feat(utilities): add omitKeysFromObject utility function (#700)
    * feat(utilities): add omitKeysFromObject utility function
    
    ---------
    
    Co-authored-by: Aura <kyradiscord@gmail.com>

---
+++ packages/utilities/src/lib/omitKeysFromObject.ts
@@ -0,0 +1,24 @@
+import { deepClone } from './deepClone';
+
+/**
+ * Clones the source object using {@link deepClone} then deletes the specified keys with {@link Reflect.deleteProperty}
+ *
+ * @template TO - The object type.
+ * @template KTO - The keys of the object type.
+ *
+ * @param source - The input object.
+ * @param keys - The keys to omit from the object.
+ * @returns A new object without the specified keys.
+ */
+export function omitKeysFromObject<Object extends object, ObjectKeys extends keyof Object>(
+	source: Object,
+	...keys: readonly ObjectKeys[]
+): Omit<Object, ObjectKeys> {
+	const clone = deepClone(source);
+
+	for (const key of keys) {
+		Reflect.deleteProperty(clone, key);
+	}
+
+	return clone;
+}
