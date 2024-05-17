41710448ae38945e36925f67c345cf3d1ebdd138
Jeroen Claassens <support@favware.tech>
2024-05-18 00:20:24 +0200

    chore: nojira: no cry

--- packages/utilities/src/lib/hasAtLeastOneKeyInObject.ts
+++ packages/utilities/src/lib/hasAtLeastOneKeyInObject.ts
@@ -5,7 +5,7 @@ import type { NonNullObject } from './types';
  * Checks whether any of the {@link keys} are in the {@link obj}
  * @param obj - The object to check
  * @param keys The keys to find in the object
- * @returns `true` if at least one of the {@link keys} is in the {@link object}, `false` otherwise.
+ * @returns `true` if at least one of the {@link keys} is in the {@link obj}, `false` otherwise.
  *
  * @example
  * ```typescript
