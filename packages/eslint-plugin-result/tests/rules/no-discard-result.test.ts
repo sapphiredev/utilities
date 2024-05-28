b687c687c495f8f0f9ef194ecf8e81d86385fd21
Jeroen Claassens <support@favware.tech>
2024-05-29 01:02:52 +0200

    test(eslint-plugin-result): split code across test cases

--- packages/eslint-plugin-result/tests/rules/no-discard-result.test.ts
+++ packages/eslint-plugin-result/tests/rules/no-discard-result.test.ts
@@ -1,6 +1,6 @@
+import { RuleTester } from '@typescript-eslint/rule-tester';
 import { fileURLToPath } from 'url';
 import { noDiscardResult } from '../../src/rules/no-discard-result';
-import { RuleTester } from '@typescript-eslint/rule-tester';
 
 export const ruleTester = new RuleTester({
 	parserOptions: {
@@ -11,11 +11,12 @@ export const ruleTester = new RuleTester({
 	parser: '@typescript-eslint/parser'
 });
 
-describe.skip('ESLint plugin result', () => {
-	ruleTester.run('no-discard-result', noDiscardResult, {
-		valid: [
-			{
-				code: `import { Result } from '@sapphire/result';
+describe('ESLint plugin result', () => {
+	test('GIVEN valid results THEN asserts that no-discard passes successfully', () => {
+		ruleTester.run('no-discard-result', noDiscardResult, {
+			valid: [
+				{
+					code: `import { Result } from '@sapphire/result';
 					function foo(): Result<string, string> {}
 					async function bar(): Promise<Result<string, string>> {}
 					const x = foo();
@@ -23,98 +24,106 @@ describe.skip('ESLint plugin result', () => {
 					y = z = await bar();
 					const complex = foo() && (((Math.random() > 0.5 ? foo() : await bar()) || foo()) ?? await bar());
 				`,
-				name: 'Result is not discarded'
-			},
-			{
-				code: `import { Result } from '@sapphire/result';
+					name: 'Result is not discarded'
+				},
+				{
+					code: `import { Result } from '@sapphire/result';
 					function foo(): Result<string, string> {}
 					void foo();
 				`,
-				name: 'Result is intentionally discarded'
-			},
-			{
-				code: `import { Result } from '@sapphire/result';
+					name: 'Result is intentionally discarded'
+				},
+				{
+					code: `import { Result } from '@sapphire/result';
 					function foo(): Result<string, string> {}
 					function bar(result: Result<string, string>) {}
 					void bar(foo());
 				`,
-				name: 'Result is passed into another function'
-			}
-		],
-		invalid: [
-			{
-				code: `import { Result } from '@sapphire/result';
+					name: 'Result is passed into another function'
+				}
+			],
+			invalid: []
+		});
+	});
+
+	test('GIVEN invalid results THEN asserts that no-discard causes an error', () => {
+		ruleTester.run('no-discard-result', noDiscardResult, {
+			valid: [],
+			invalid: [
+				{
+					code: `import { Result } from '@sapphire/result';
 					function foo(): Result<string, string> {}
 
 					foo();`,
-				name: 'simple discard',
-				errors: [
-					{
-						messageId: 'discardedResult',
-						line: 4
-					}
-				]
-			},
-			{
-				code: `import { Result } from '@sapphire/result';
+					name: 'simple discard',
+					errors: [
+						{
+							messageId: 'discardedResult',
+							line: 4
+						}
+					]
+				},
+				{
+					code: `import { Result } from '@sapphire/result';
 					async function foo(): Promise<Result<string, string>> {}
 
 					foo();`,
-				name: 'unawaited async function discarded',
-				errors: [
-					{
-						messageId: 'discardedResult',
-						line: 4
-					}
-				]
-			},
-			{
-				code: `import { Result } from '@sapphire/result';
+					name: 'unawaited async function discarded',
+					errors: [
+						{
+							messageId: 'discardedResult',
+							line: 4
+						}
+					]
+				},
+				{
+					code: `import { Result } from '@sapphire/result';
 					async function foo(): Promise<Result<string, string>> {}
 
 					await foo();`,
-				name: 'awaited async function discarded',
-				errors: [
-					{
-						messageId: 'discardedResult',
-						line: 4
-					}
-				]
-			},
-			{
-				code: `import { Result } from '@sapphire/result';
+					name: 'awaited async function discarded',
+					errors: [
+						{
+							messageId: 'discardedResult',
+							line: 4
+						}
+					]
+				},
+				{
+					code: `import { Result } from '@sapphire/result';
 					function foo(): Promise<Result<string, string>> {}
 
 					(
 						foo(),
 						await foo()
 					);`,
-				name: 'double discard',
-				errors: [
-					{
-						messageId: 'discardedResult',
-						line: 5
-					},
-					{
-						messageId: 'discardedResult',
-						line: 6
-					}
-				]
-			},
-			{
-				code: `import { Result } from '@sapphire/result';
+					name: 'double discard',
+					errors: [
+						{
+							messageId: 'discardedResult',
+							line: 5
+						},
+						{
+							messageId: 'discardedResult',
+							line: 6
+						}
+					]
+				},
+				{
+					code: `import { Result } from '@sapphire/result';
 					function foo(): Promise<Result<string, string>> {}
 
 					null ?? foo();
 					`,
-				name: 'potential discard (??)',
-				errors: [
-					{
-						messageId: 'discardedResult',
-						line: 4
-					}
-				]
-			}
-		]
+					name: 'potential discard (??)',
+					errors: [
+						{
+							messageId: 'discardedResult',
+							line: 4
+						}
+					]
+				}
+			]
+		});
 	});
 });
