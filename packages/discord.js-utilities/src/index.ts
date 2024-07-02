9f854b2ddef249a5fe8ee43c43baee13d571276b
Jeroen Claassens <support@favware.tech>
2024-07-02 16:01:01 +0200

    feat(discord.js-utilities)!: remove PaginatedMessage code from package
    BREAKING CHANGE: PaginatedMessage has been moved to separate package @sapphire/paginated-message

--- packages/discord.js-utilities/src/index.ts
+++ packages/discord.js-utilities/src/index.ts
@@ -1,7 +1,6 @@
 export * from '@sapphire/discord-utilities';
 export * from './lib/builders/MessageBuilder';
 export * from './lib/MessagePrompter';
-export * from './lib/PaginatedMessages';
 export * from './lib/type-guards';
 export * from './lib/utilities';
 export type * from './lib/utility-types';
--- packages/discord.js-utilities/src/lib/PaginatedMessages/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './LazyPaginatedMessage';
-export * from './PaginatedFieldMessageEmbed';
-export * from './PaginatedMessage';
-export * from './PaginatedMessageEmbedFields';
-export * from './PaginatedMessageTypes';
-export * from './utils';
