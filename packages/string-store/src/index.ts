cd5652452d4f234efab30acc6937e923a7ac7bf8
Aura Román <kyradiscord@gmail.com>
2024-07-26 20:43:49 +0200

    fix: resolve `gen-index` script error

--- packages/string-store/src/index.ts
+++ packages/string-store/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/schema/Schema';
 export * from './lib/schema/SchemaStore';
 export * from './lib/shared/Pointer';
-export * from './lib/types/index';
+export * from './lib/types';
 export * from './lib/UnalignedUint16Array';
