5ac3f24679bfcc914abd2da5ae460b24197e3458
Aura <kyradiscord@gmail.com>
2024-10-27 16:07:28 +0100

    feat: add `PrettifyObject` type (#822)
    * feat: add `PrettifyObject` type
    
    * fix: lint

--- packages/utilities/src/lib/types.ts
+++ packages/utilities/src/lib/types.ts
@@ -132,6 +132,33 @@ export type AnyObject<T> = {
 };
 
 /**
+ * An utility type that fuses intersections of objects.
+ *
+ * @example
+ *
+ * ```typescript
+ * type Objects = {
+ *   foo: string;
+ *   bar: number;
+ * } & {
+ *   hello: boolean;
+ *   world: bigint;
+ * };
+ *
+ * type PrettyObjects = PrettifyObject<Objects>;
+ * // {
+ * //   foo: string;
+ * //   bar: number;
+ * //   hello: boolean;
+ * //   world: bigint
+ * // }
+ * ```
+ */
+export type PrettifyObject<T extends object> = {
+	[K in keyof T]: T[K];
+};
+
+/**
  * Gets all the keys (as a string union) from a type `T` that match value `V`
  * @example
  * ```typescript
