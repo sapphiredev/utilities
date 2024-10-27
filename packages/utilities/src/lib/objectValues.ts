d51a8a52b5ea8c11af1dbb68566283e59c9610fd
Aura Román <kyradiscord@gmail.com>
2024-10-27 13:41:36 +0100

    refactor: deprecate `NonNullObject` and `AnyObject`

--- packages/utilities/src/lib/objectValues.ts
+++ packages/utilities/src/lib/objectValues.ts
@@ -1,5 +1,3 @@
-import type { NonNullObject } from './types';
-
-export function objectValues<T extends NonNullObject>(obj: T): T extends ArrayLike<any> ? T[number][] : T[keyof T][] {
+export function objectValues<T extends object>(obj: T): T extends ArrayLike<any> ? T[number][] : T[keyof T][] {
 	return Object.values(obj) as T extends ArrayLike<infer Values> ? Values[] : T[keyof T][];
 }
