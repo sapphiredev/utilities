34ac0ceca9067b368bb1b0235fb41cccbfc7abbd
Aura <kyradiscord@gmail.com>
2024-11-22 09:37:58 +0000

    fix: test names

--- packages/iterator-utilities/tests/windows.test.ts
+++ packages/iterator-utilities/tests/windows.test.ts
@@ -1,21 +1,21 @@
 import { windows } from '../src';
 
 describe('windows', () => {
-	test('GIVEN iterable and limit greater than iterable length THEN returns entire iterable', () => {
+	test('GIVEN iterable and limit greater than iterable length THEN returns an empty iterable', () => {
 		const iterable = [1, 2, 3];
 		const limit = 5;
 		const result = [...windows(iterable, limit)];
 		expect(result).toEqual([]);
 	});
 
-	test('GIVEN iterable and limit equal to iterable length THEN returns entire iterable', () => {
+	test('GIVEN iterable and limit equal to iterable length THEN returns an array with the contents of the iterable', () => {
 		const iterable = [1, 2, 3];
 		const limit = 3;
 		const result = [...windows(iterable, limit)];
 		expect(result).toEqual([[1, 2, 3]]);
 	});
 
-	test('GIVEN iterable and limit less than iterable length THEN returns limited iterable', () => {
+	test('GIVEN iterable and limit less than iterable length THEN returns multiple windows', () => {
 		const iterable = [1, 2, 3];
 		const limit = 2;
 		const result = [...windows(iterable, limit)];
@@ -32,7 +32,7 @@ describe('windows', () => {
 		expect(result).toEqual([]);
 	});
 
-	test('GIVEN iterable and limit equal to 0 THEN returns empty iterable', () => {
+	test('GIVEN iterable and limit equal to 0 THEN throws RangeError', () => {
 		const iterable = [1, 2, 3];
 		const limit = 0;
 		expect(() => windows(iterable, limit)).toThrowError(new RangeError('0 must be a positive number'));
