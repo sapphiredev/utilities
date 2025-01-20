e506d2a0f06cb102b96ab6ae2a11fab5c834d246
GeniusTimo <mail@geniustimo.de>
2025-01-20 18:38:53 +0100

    test(utilities): Added some additional test cases
    See https://github.com/sapphiredev/utilities/pull/857#pullrequestreview-2560993219

--- packages/utilities/tests/objectKeys.test.ts
+++ packages/utilities/tests/objectKeys.test.ts
@@ -15,17 +15,31 @@ describe('objectKeys', () => {
 		expect<('a' | 'b' | 'deep')[]>(objectKeys(source)).toEqual(expected);
 	});
 
+	test('GIVEN array readonly THEN returns expected', () => {
+		const source = ['Hello', 420] as const;
+		const expected = ['0', '1'];
+
+		expect<`${number}`[]>(objectKeys(source)).toEqual(expected);
+	});
+
 	test('GIVEN basic THEN returns expected', () => {
 		const source = { a: 'Hello', b: 420 };
 		const expected = ['a', 'b'];
 
-		expect(objectKeys(source)).toEqual(expected);
+		expect<('a' | 'b')[]>(objectKeys(source)).toEqual(expected);
 	});
 
 	test('GIVEN deep THEN returns expected', () => {
 		const source = { a: 'Hello', b: 420, deep: { i: [] } };
 		const expected = ['a', 'b', 'deep'];
 
-		expect(objectKeys(source)).toEqual(expected);
+		expect<('a' | 'b' | 'deep')[]>(objectKeys(source)).toEqual(expected);
+	});
+
+	test('GIVEN array THEN returns expected', () => {
+		const source = ['Hello', 420];
+		const expected = ['0', '1'];
+
+		expect<`${number}`[]>(objectKeys(source)).toEqual(expected);
 	});
 });
--- packages/utilities/tests/objectValues.test.ts
+++ packages/utilities/tests/objectValues.test.ts
@@ -15,17 +15,29 @@ describe('objectValues', () => {
 		expect<('Hello' | 420 | { i: readonly [] })[]>(objectValues(source)).toEqual(expected);
 	});
 
-	test('GIVEN basic readonly THEN returns expected', () => {
+	test('GIVEN array readonly THEN returns same', () => {
+		const source = ['Hello', 420] as const;
+
+		expect<('Hello' | 420)[]>(objectValues(source)).toEqual(source);
+	});
+
+	test('GIVEN basic THEN returns expected', () => {
 		const source = { a: 'Hello', b: 420 };
 		const expected = ['Hello', 420];
 
-		expect(objectValues(source)).toEqual(expected);
+		expect<(string | number)[]>(objectValues(source)).toEqual(expected);
 	});
 
 	test('GIVEN deep THEN returns expected', () => {
 		const source = { a: 'Hello', b: 420, deep: { i: [] } };
 		const expected = ['Hello', 420, { i: [] }];
 
-		expect(objectValues(source)).toEqual(expected);
+		expect<(string | number | { i: never[] })[]>(objectValues(source)).toEqual(expected);
+	});
+
+	test('GIVEN array THEN returns same', () => {
+		const source = ['Hello', 420];
+
+		expect<(string | number)[]>(objectValues(source)).toEqual(source);
 	});
 });
