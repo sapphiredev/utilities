15184bad08fad49e861bebafbe0b21e0da2a462b
Sammy <45788465+SammyWhamy@users.noreply.github.com>
2025-12-14 12:49:45 +0100

    fix(string-store)!: use only valid unicode characters to encode (#935)

---
+++ packages/string-store/src/lib/shared/codepoints.ts
@@ -0,0 +1,20 @@
+export const codepointRanges = [
+	{
+		start: 0x4e00,
+		end: 0x9ffc,
+		indexStart: 0,
+		indexEnd: 20988
+	},
+	{
+		start: 0x17000,
+		end: 0x187f7,
+		indexStart: 20989,
+		indexEnd: 27125
+	},
+	{
+		start: 0x20000,
+		end: 0x2960a,
+		indexStart: 27126,
+		indexEnd: 65536
+	}
+];
