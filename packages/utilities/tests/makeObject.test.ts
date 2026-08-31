17c2bfe6de648957bf73065a77f5f22e0070bac7
favna <support@favware.tech>
2026-08-31 12:34:14 +0200

    fix(makeobject): resolve prototype pollution
    Signed-off-by: favna <support@favware.tech>

--- packages/utilities/tests/makeObject.test.ts
+++ packages/utilities/tests/makeObject.test.ts
@@ -20,4 +20,23 @@ describe('makeObject', () => {
 		const made = makeObject('he.wor', 'ld', { he: { llo: 'world' } });
 		expect(made).toEqual({ he: { llo: 'world', wor: 'ld' } });
 	});
+
+	test.each(['__proto__.polluted', 'constructor.prototype.polluted', 'safe.prototype.polluted'])(
+		'GIVEN path containing reserved segment %s THEN throws without polluting prototypes',
+		(path) => {
+			const object = {};
+
+			expect(() => makeObject(path, true, object)).toThrowError(new TypeError('path cannot contain __proto__, constructor, or prototype'));
+			expect(Object.prototype).not.toHaveProperty('polluted');
+			expect(object).toEqual({});
+		}
+	);
+
+	test('GIVEN inherited path segment THEN creates an own object', () => {
+		const made = makeObject('toString.polluted', true);
+
+		expect(made).toEqual({ toString: { polluted: true } });
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(Object.prototype.toString).not.toHaveProperty('polluted');
+	});
 });
